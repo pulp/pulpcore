d24c1b7d50bd71e3c9f0cadfb7db5750314eb5ac
Austin Macdonald <austin@redhat.com>
2019-03-18 15:06:52 -0400

    postgres
    [noissue]

--- .travis/install.sh
+++ .travis/install.sh
@@ -13,4 +13,8 @@ cd ../ansible-pulp3
 ansible-galaxy install -r requirements.yml
 ansible-playbook --connection=local --inventory 127.0.0.1, playbook.yml --extra-vars "pulp_python_interpreter=$VIRTUAL_ENV/bin/python, pulp_install_dir=$VIRTUAL_ENV"
 
+# false for both postgresql and maria
+# pulp_install_postgresql: false
+# pulp_install_maria: false
+    pulp_postgresql_user: 'travis'
 cd ../pulpcore
