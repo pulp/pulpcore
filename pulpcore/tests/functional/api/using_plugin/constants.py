7faa5ea4ee72f5e0141e2aaea907bbf9b9319d5a
ragabala <ragavendranb92@gmail.com>
2019-03-20 12:03:27 -0400

    Adding mirror true check for rpm/file plugin
    This commit will test cross plugin scenario. If the user syncs multiple
    plugin contents into the same repo with the option mirror=True, the
    existing content should be overwritten.
    
    closes #4448

--- pulpcore/tests/functional/api/using_plugin/constants.py
+++ pulpcore/tests/functional/api/using_plugin/constants.py
@@ -65,3 +65,31 @@ FILE_URL = urljoin(FILE_FIXTURE_URL, '1.iso')
 
 FILE2_URL = urljoin(FILE2_FIXTURE_URL, '1.iso')
 """The URL to an ISO file at :data:`FILE2_FIXTURE_URL`."""
+
+
+RPM_PACKAGE_CONTENT_NAME = 'rpm.package'
+
+RPM_UPDATE_CONTENT_NAME = 'rpm.update'
+
+RPM_REMOTE_PATH = urljoin(BASE_REMOTE_PATH, 'rpm/rpm/')
+
+RPM_UNSIGNED_FIXTURE_URL = urljoin(PULP_FIXTURES_BASE_URL, 'rpm-unsigned/')
+"""The URL to a repository with unsigned RPM packages."""
+
+RPM_PACKAGES_COUNT = 35
+"""The number of packages available at
+:data:`RPM_SIGNED_FIXTURE_URL` and :data:`RPM_UNSIGNED_FIXTURE_URL`
+"""
+
+RPM_UPDATE_COUNT = 4
+"""The number of updated record units."""
+
+RPM_FIXTURE_SUMMARY = {
+    RPM_PACKAGE_CONTENT_NAME: RPM_PACKAGES_COUNT,
+    RPM_UPDATE_CONTENT_NAME: RPM_UPDATE_COUNT
+}
+"""The breakdown of how many of each type of content unit are present in the
+standard repositories, i.e. :data:`RPM_SIGNED_FIXTURE_URL` and
+:data:`RPM_UNSIGNED_FIXTURE_URL`.  This matches the format output by the
+"content_summary" field on "../repositories/../versions/../".
+"""
