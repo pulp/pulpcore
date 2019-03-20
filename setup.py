9e35a1c0c9b9676ee5dee61826b332e1610f9c4c
Dennis Kliban <dkliban@redhat.com>
2019-03-20 16:57:56 -0400

    Beta 23 release notes and version bump
    [noissue]

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ requirements = [
 
 setup(
     name='pulpcore',
-    version='3.0.0b22',
+    version='3.0.0b23',
     description='Pulp Django Application and Related Modules',
     long_description=long_description,
     long_description_content_type="text/markdown",
