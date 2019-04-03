935d2c0a0d875786def231f52db377d164c755a6
koliveir <koliveir@redhat.com>
2019-04-03 15:30:55 -0400

    Update distribution creation to use task_handler
    Distribution creation it was modified to be an async task. A new
    response_handler was added to pulp-smash to deal with those cases.
    Update distribution creation to use this new response_handler.
    
    '[noissue]'

--- pulpcore/tests/functional/api/using_plugin/test_content_promotion.py
+++ pulpcore/tests/functional/api/using_plugin/test_content_promotion.py
@@ -74,10 +74,10 @@ class ContentPromotionTestCase(unittest.TestCase):
         for _ in range(2):
             body = gen_distribution()
             body['publication'] = publication['_href']
-            response_dict = client.post(DISTRIBUTION_PATH, body)
-            dist_task = client.get(response_dict['task'])
-            distribution_href = dist_task['created_resources'][0]
-            distribution = client.get(distribution_href)
+            distribution = client.using_handler(api.task_handler).post(
+                DISTRIBUTION_PATH,
+                body
+            )
             distributions.append(distribution)
             self.addCleanup(client.delete, distribution['_href'])
 
