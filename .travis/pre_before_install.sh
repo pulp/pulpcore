f5ef71c8d5493e3da087525bf3a0c69f07517225
Austin Macdonald <austin@redhat.com>
2019-06-27 14:24:14 -0400

    Exploratory commit, is COMMIT_MSG being set?
    Required PR: https://github.com/pulp/pulp_file/pull/250
    
    [noissue]

--- .travis/pre_before_install.sh
+++ .travis/pre_before_install.sh
@@ -1,6 +1,12 @@
 export PULP_FILE_PR_NUMBER=$(echo $COMMIT_MSG | grep -oP 'Required\ PR:\ https\:\/\/github\.com\/pulp\/pulp_file\/pull\/(\d+)' | awk -F'/' '{print $7}')
 export PULP_CERTGUARD_PR_NUMBER=$(echo $COMMIT_MSG | grep -oP 'Required\ PR:\ https\:\/\/github\.com\/pulp\/pulp-certguard\/pull\/(\d+)' | awk -F'/' '{print $7}')
 
+
+echo "PULP_FILE_PR_NUMER:"
+echo $PULP_FILE_PR_NUMER
+echo "\n\nCOMMIT_MSG:"
+echo $COMMIT_MSG
+
 cd ..
 git clone https://github.com/pulp/pulp_file.git
 if [ -n "$PULP_FILE_PR_NUMBER" ]; then
