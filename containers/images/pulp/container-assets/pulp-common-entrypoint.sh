3e786f2de9b7c3e14a96b5dd12d1699ea01866fe
Mike DePaulo <mikedep333@redhat.com>
2019-07-23 14:15:18 -0400

    containers: Switch to Fedora 30
    Includes:
    
    A Fix for launching pulp-content on Fedora,
    where /usr/local/bin/pulp-content is a pip-installed script
    in the PATH, ahead of /usr/bin/pulp-content .
    
    Making the pulp-resource-manager & pulp-worker scripts more consistent
    
    fixes #5176

--- containers/images/pulp/container-assets/pulp-common-entrypoint.sh
+++ containers/images/pulp/container-assets/pulp-common-entrypoint.sh
@@ -2,4 +2,10 @@
 
 export DJANGO_SETTINGS_MODULE=pulpcore.app.settings
 
-exec "$@"
+# Prevent pip-installed /usr/local/bin/pulp-content from getting run instead of
+# our /usr/bin/pulp script.
+#
+# We still want conatiner users to call command names, not paths, so we can
+# change our scripts' locations in the future, and call special logic in this
+# script based solely on the command name.
+exec "/usr/bin/$@"
