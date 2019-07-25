20e0172db4ae6d39cad359d10267f911d71ec4f6
Brian Bouterse <bbouters@redhat.com>
2019-07-25 09:18:24 -0400

    Version bump to 3.0.0rc5.dev
    [noissue]

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ requirements = [
 
 setup(
     name='pulpcore',
-    version='3.0.0rc4',
+    version='3.0.0rc5.dev',
     description='Pulp Django Application and Related Modules',
     long_description=long_description,
     long_description_content_type="text/markdown",
