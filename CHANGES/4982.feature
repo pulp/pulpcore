64da09737e25969938e7a98dce4bbe659e6ea5be
Brian Bouterse <bbouters@redhat.com>
2019-07-25 08:31:01 -0400

    RC4 release
    [noissue]

--- CHANGES/4982.feature
+++
@@ -1,1 +0,0 @@
-Allow users to pass sha256 with each chunk to have Pulp verify the chunk.
--- pulpcore/__init__.py
+++ pulpcore/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '3.0.0rc4.dev'
+__version__ = '3.0.0rc4'
 
 
 from pkgutil import extend_path
