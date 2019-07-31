17ccb3eea9dd6ab6ca4521587343426baf30b4b5
Mike DePaulo <mikedep333@redhat.com>
2019-07-31 15:08:17 -0400

    containers: Build both stable and dev images
    and make images far more configurable.
    
    Implementation includes:
    
    Build stable and master (latest) versions (tags) of both images.
    
    Use an undefined plugins list for no plugins ather than the
    dummy pulpcore-plugin.
    
    Use variables for the pulpcore & pulpcore-plugin pip installs
    
    Use vars/vars.yml as an override for vars
    (including the images data structure.)
    
    Start work on installing into the container, and limit the CI builds
    to only pulpcore & pulpcore-plugin stable.
    
    fixes: #5116
    Handle properly whether to build container images with pulpcore stable or dev
    (Actually does more than that.)
    https://pulp.plan.io/issues/5116
    
    Includes some work on:
    re: #5062
    Create pulpcore and pulp_file container images automatically via CI
    https://pulp.plan.io/issues/5062

--- .travis/post_script.sh
+++ .travis/post_script.sh
@@ -2,4 +2,32 @@
 
 cd containers
 
+# FIXME: This won't work  because not only is it outside the Docker build env,
+# it's outside of the build context (the pwd from which `docker build` is run.)
+# We need to find/develop a Dockerfile COPY solution for this.
+: '
+if [ -n "$PULP_PLUGIN_PR_NUMBER" ]; then
+  PULPCORE_PLUGIN_PATH=../../pulpcore-plugin
+else
+  PULPCORE_PLUGIN_PATH=git+https://github.com/pulp/pulpcore-plugin.git
+fi
+cat > vars/vars.yaml << VARSYAML
+---
+images:
+  - pulpcore_$(git rev-parse --abbrev-ref HEAD | tr '-' '_'):
+      image_name: pulpcore
+      tag: $(git rev-parse --abbrev-ref HEAD)
+      pulpcore: ..
+      pulpcore_plugin: $PULPCORE_PLUGIN_PATH
+VARSYAML
+'
+
+cat > vars/vars.yaml << VARSYAML
+---
+images:
+  - pulpcore_$(git rev-parse --abbrev-ref HEAD | tr '-' '_'):
+      image_name: pulpcore
+      tag: $(git rev-parse --abbrev-ref HEAD)
+VARSYAML
+
 ansible-playbook build.yaml
