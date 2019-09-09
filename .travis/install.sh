dd116478bdc47cdf986858dfab18cbf65d30d6f9
Mike DePaulo <mikedep333@redhat.com>
2019-09-09 14:20:22 -0400

    Problem: CI fails with dependabot, which uses a / in git branch names
    Solution: regenerate with latest plugin-template
    
    [noissue]

--- .travis/install.sh
+++ .travis/install.sh
@@ -20,18 +20,28 @@ pip install -r test_requirements.txt
 
 cd $TRAVIS_BUILD_DIR/../pulpcore/containers/
 
+# Although the tag name is not used outside of this script, we might use it
+# later. And it is nice to have a friendly identifier for it.
+# So we use the branch preferably, but need to replace the "/" with the valid
+# character "_" .
+#
+# Note that there are lots of other valid git branch name special characters
+# that are invalid in image tag names. To try to convert them, this would be a
+# starting point:
+# https://stackoverflow.com/a/50687120
+#
 # If we are on a PR
 if [ -n "$TRAVIS_PULL_REQUEST_BRANCH" ]; then
-  TAG=$TRAVIS_PULL_REQUEST_BRANCH
+  TAG=$(echo $TRAVIS_PULL_REQUEST_BRANCH | tr / _)
 # For push builds, tag builds, and hopefully cron builds
 elif [ -n "$TRAVIS_BRANCH" ]; then
-  TAG=$TRAVIS_BRANCH
+  TAG=$(echo $TRAVIS_BRANCH | tr / _)
   if [ "$TAG" = "master" ]; then
     TAG=latest
   fi
 else
   # Fallback
-  TAG=$(git rev-parse --abbrev-ref HEAD)
+  TAG=$(git rev-parse --abbrev-ref HEAD | tr / _)
 fi
 
 
