5fb05ee86a18edece5c7587cfb0b9357845036c2
Matthias Dellweg <dellweg@atix.de>
2019-09-11 10:06:10 +0200

    Refactor upload serializers
    [noissue]

--- pulpcore/app/serializers/upload.py
+++ pulpcore/app/serializers/upload.py
@@ -1,3 +1,5 @@
+import re
+
 from gettext import gettext as _
 from rest_framework import serializers
 
@@ -5,22 +7,51 @@ from pulpcore.app import models
 from pulpcore.app.serializers import base
 
 
+CONTENT_RANGE_PATTERN = r'^bytes (\d+)-(\d+)/(\d+|[*])$'
+
+
 class UploadChunkSerializer(serializers.Serializer):
     file = serializers.FileField(
         help_text=_("A chunk of the uploaded file."),
+        write_only=True,
     )
 
     sha256 = serializers.CharField(
         help_text=_("The SHA-256 checksum of the chunk if available."),
         required=False,
         allow_null=True,
+        write_only=True,
     )
 
+    offset = serializers.IntegerField(
+        read_only=True,
+    )
 
-class UploadChunkDetailSerializer(base.ModelSerializer):
+    size = serializers.IntegerField(
+        read_only=True,
+    )
+
+    def validate(self, data):
+        data = super().validate(data)
+
+        content_range = self.context["request"].META.get('HTTP_CONTENT_RANGE', '')
+        match = re.compile(CONTENT_RANGE_PATTERN).match(content_range)
+        if not match:
+            raise serializers.ValidationError(_("Invalid or missing content range header."))
+        data["start"] = start = int(match[1])
+        end = int(match[2])
+
+        if (end - start + 1) != len(data["file"]):
+            raise serializers.ValidationError(_("Chunk size does not match content range."))
+
+        if end > self.context["upload"].size - 1:
+            raise serializers.ValidationError(_("End byte is greater than upload size."))
+
+        return data
+
     class Meta:
         model = models.UploadChunk
-        fields = ('offset', 'size')
+        fields = ('file', 'sha256', 'offset', 'size')
 
 
 class UploadSerializer(base.ModelSerializer):
@@ -44,14 +75,13 @@ class UploadSerializer(base.ModelSerializer):
 
 
 class UploadDetailSerializer(UploadSerializer):
-    chunks = UploadChunkDetailSerializer(
+    chunks = UploadChunkSerializer(
         many=True,
         read_only=True,
     )
 
-    class Meta:
-        model = models.Upload
-        fields = base.ModelSerializer.Meta.fields + ('size', 'completed', 'chunks')
+    class Meta(UploadSerializer.Meta):
+        fields = UploadSerializer.Meta.fields + ('chunks',)
 
 
 class UploadCommitSerializer(serializers.Serializer):
