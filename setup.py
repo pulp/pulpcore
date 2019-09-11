5cead4d09958379a11cf1c9bf3abf571dc73ed1c
David Davis <daviddavis@redhat.com>
2019-09-11 11:24:33 -0400

    Bumping release for rc6
    [noissue]

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ requirements = [
 
 setup(
     name='pulpcore',
-    version='3.0.0rc5',
+    version='3.0.0rc6.dev',
     description='Pulp Django Application and Related Modules',
     long_description=long_description,
     long_description_content_type="text/markdown",
