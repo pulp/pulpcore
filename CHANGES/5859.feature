a15d269c824a83c39a5a6b82e5ffbab090b76f4a
Evgeni Golov <evgeni@golov.de>
2019-12-10 10:17:59 +0100

    provide a pulpcore-admin script
    closes #5859
    https://pulp.plan.io/issues/5859

---
+++ CHANGES/5859.feature
@@ -0,0 +1,1 @@
+Added a `pulpcore-admin` script that can be used to call `manage.py`.
--- setup.py
+++ setup.py
@@ -50,4 +50,7 @@ setup(
         'Programming Language :: Python :: 3.7',
     ),
     scripts=['bin/pulp-content'],
+    entry_points={'console_scripts': [
+        'pulpcore-admin = pulpcore.app.manage:manage',
+    ]}
 )
