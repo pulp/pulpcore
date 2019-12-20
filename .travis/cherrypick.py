c1b43b91523c41b73e0ed94920945008d93ebe63
David Davis <daviddavis@redhat.com>
2019-12-20 07:59:15 -0500

    Updating the cherry pick script.
    ref #5795

--- .travis/cherrypick.py
+++ .travis/cherrypick.py
@@ -11,6 +11,52 @@ REPOSITORY = os.environ["TRAVIS_REPO_SLUG"]
 GITHUB_USER = os.environ["GITHUB_USER"]
 GITHUB_TOKEN = os.environ["GITHUB_TOKEN"]
 
+
+def blobs_match(commit1, commit2):
+    """Compare the file blobs of two commits"""
+    if len(commit1.files) != len(commit2.files):
+        return False
+
+    for cf in commit1.files:
+        files = [f for f in commit2.files if f.sha == cf.sha and f.filename == cf.filename]
+        if len(files) == 0:
+            return False
+
+    return True
+
+
+def get_merged_commits(pr):
+    """
+    Find the merged commits for a PR.
+
+    Github allows PRs to be merged with 3 different strategies: merge, rebase, and squashing. Github
+    doesn't record how a PR was merged in the API so we need to examine the merge commit to
+    determine how the pr was merged and then return the commits based on that.
+    """
+    merge_commit = pr.base.repo.get_commit(pr.merge_commit_sha)
+
+    if len(merge_commit.parents) > 1:
+        # PR was just merged and we can use the PR's commits
+        return pr.get_commits()
+    elif blobs_match(merge_commit, pr.get_commits().reversed[0]):
+        # if the last PR commit is the same as the merge commit then the PR was rebased
+        commits = []
+        commit = merge_commit
+        for _ in pr.get_commits():
+            if len(commit.parents) > 1:
+                print(f"Ran into problem attempting to cherry pick {commit.sha}.")
+                exit(1)
+            commits.append(commit)
+            commit = commit.parents[0]
+        commits.reverse()
+        return commits
+    else:
+        # we have a squashed commit. we can just use the merge commit
+        return merge_commit
+
+
+print(f"Processing cherry picks for {REPOSITORY}.")
+
 # prepare our local repo to receive git cherry picks
 repo = Repo(os.getcwd())
 remote_url = f"https://{GITHUB_USER}:{GITHUB_TOKEN}@github.com/{REPOSITORY}.git"
@@ -29,15 +75,16 @@ for issue in issues:
     try:
         pr = grepo.get_pull(issue.number)
     except UnknownObjectException:
+        # this issue is not a PR. skip it.
         continue
 
     if not pr.merged:
         print(f"Pull request {pr.number} not merged. Skipping.")
         continue
 
-    print(f"Attempting to cherry-pick commits for PR {pr.number}.")
+    print(f"Attempting to cherry-pick commits for {pr.html_url}.")
 
-    for commit in pr.get_commits():
+    for commit in get_merged_commits(pr):
         # looks like GitPython doesn't support cherry picks
         ret = subprocess.run(["git", "cherry-pick", "-x", commit.sha], stderr=subprocess.PIPE)
 
