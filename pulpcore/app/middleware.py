f5550cefb98e5d2b6ba5559b324489e3640dc504
Brian Bouterse <bmbouter@redhat.com>
2020-01-14 12:23:52 -0500

    REMOTE_USER auth shouldn't trigger csrf rejects
    Move all authentication fully to DRF. We were incorrectly configuring
    webserver auth support in django and not DRF. This ports the setting
    `REMOTE_USER_ENVIRON_NAME` to work with DRF instead of Django.
    
    It adjusts the settings.py so there are removal claims even though this
    will likely go into a z-release. It's an important FYI for the user.
    
    https://pulp.plan.io/issues/5955
    closes #5955
    
    (cherry picked from commit 1ae65bde73d649ac778357608a5ea84d48fd2096)

--- pulpcore/app/middleware.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.middleware import RemoteUserMiddleware
-
-from pulpcore.app import settings
-
-
-class PulpRemoteUserMiddleware(RemoteUserMiddleware):
-    header = settings.REMOTE_USER_ENVIRON_NAME
