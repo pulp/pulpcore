a4ada59cc39cc17edf70c6fb655c7182eb9540ba
Brian Bouterse <bmbouter@redhat.com>
2020-01-15 17:09:32 -0500

    Version bump to 3.0.2.dev for the next z-stream
    [noissue]

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ requirements = [
 
 setup(
     name='pulpcore',
-    version='3.0.1',
+    version='3.0.2.dev',
     description='Pulp Django Application and Related Modules',
     long_description=long_description,
     long_description_content_type="text/markdown",
