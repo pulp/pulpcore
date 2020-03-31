7d4896a360ea7fe625eb793de214f568bcecd686
Mike DePaulo <mikedep333@redhat.com>
2020-03-31 10:00:42 -0400

    Problem: Pulp containers do not allow commands like "/bin/bash" to be run, only "bash"
    Solution: Only apply the /bin/ logic for the 4 pulp-* commands.
    
    fixes: #6420
    Pulp containers do not allow commands like "/bin/bash" to be run, only "bash"
    https://pulp.plan.io/issues/6420
    
    (cherry picked from commit 10804a79115c4dbbd745f7f82ea197f677f77c11)

--- containers/images/pulp/container-assets/pulp-common-entrypoint.sh
+++ containers/images/pulp/container-assets/pulp-common-entrypoint.sh
@@ -3,7 +3,12 @@
 # Prevent pip-installed /usr/local/bin/pulp-content from getting run instead of
 # our /usr/bin/pulp script.
 #
-# We still want conatiner users to call command names, not paths, so we can
-# change our scripts' locations in the future, and call special logic in this
-# script based solely on the command name.
-exec "/usr/bin/$@"
+# We still want conatiner users to call pulp-* command names, not paths, so we
+# can change our scripts' locations in the future, and call special logic in this
+# script based solely on theo command name.
+
+if [[ "$@" = "pulp-content" || "$@" = "pulp-api" || "$@" = "pulp-worker" || "$@" = "pulp-resource-manager" ]]; then
+        exec "/usr/bin/$@"
+else
+        exec "$@"
+fi
