04ab00ad756ed7b515d73c5c7e3f1988ed281125
Grant Gainey <ggainey@redhat.com>
2020-04-01 19:26:29 -0400

    DRAFT: Layout tweak for Repository info

--- pulpcore/app/importexport.py
+++ pulpcore/app/importexport.py
@@ -38,7 +38,7 @@ class ContentArtifactResource(resources.ModelResource):
 
 
 def export_content(export, repository_version):
-        dest = os.path.join(export.destination_dir, 'repository-{}'.format(str(repository_version.pulp_id)))
+        dest = os.path.join(export.destination_dir, 'repository-{}-{}'.format(str(repository_version.repository.pulp_id), repository_version.number))
         try:
             os.makedirs(dest)
         except FileExistsError:
@@ -65,16 +65,16 @@ def export_content(export, repository_version):
         with open(file, "w") as f:
             f.write(dataset.json)
 
-        resource = RepositoryResource()
-        queryset = Repository.objects.filter(pulp_id=repository_version.repository.pulp_id)
-        dataset = resource.export(queryset)
-        file = os.path.join(dest, 'repositoryresource.json')
-        with open(file, "w") as f:
-            f.write(dataset.json)
-
         resource = RepositoryVersionResource()
         queryset = RepositoryVersion.objects.filter(pulp_id=repository_version.pulp_id)
         dataset = resource.export(queryset)
         file = os.path.join(dest, 'repositoryversionresource.json')
+        with open(file, "w") as f:
+            f.write(dataset.json)
+
+        resource = RepositoryResource()
+        queryset = Repository.objects.filter(pk__in=export.exporter.repositories.all())
+        dataset = resource.export(queryset)
+        file = os.path.join(export.destination_dir, 'repositoryresource.json')
         with open(file, "w") as f:
             f.write(dataset.json)
