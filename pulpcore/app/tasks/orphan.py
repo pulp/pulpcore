d89a488d8e639ba330a991e970bfd4c951be7332
Daniel Alley <dalley@redhat.com>
2020-04-09 11:53:14 -0400

    Improve performance of orphan cleanup slightly
    Also memory consumption.
    
    re: #6581
    https://pulp.plan.io/issues/6581

--- pulpcore/app/tasks/orphan.py
+++ pulpcore/app/tasks/orphan.py
@@ -1,11 +1,32 @@
+import gc
+
 from pulpcore.app.models import (
     Artifact,
     Content,
-    ContentArtifact,
     ProgressReport,
     PublishedMetadata,
-    RepositoryContent,
 )
+
+
+def queryset_iterator(qs, batchsize=2000, gc_collect=True):
+    """
+    Provide a batching functionality that returns individual querysets per batch.
+
+    Copied from here with minor changes:
+    https://www.guguweb.com/2020/03/27/optimize-django-memory-usage/
+    """
+    iterator = qs.values_list("pk", flat=True).order_by("pk").distinct().iterator()
+    eof = False
+    while not eof:
+        primary_key_buffer = []
+        try:
+            while len(primary_key_buffer) < batchsize:
+                primary_key_buffer.append(next(iterator))
+        except StopIteration:
+            eof = True
+        yield qs.filter(pk__in=primary_key_buffer).order_by("pk")
+        if gc_collect:
+            gc.collect()
 
 
 def orphan_cleanup():
@@ -13,28 +34,31 @@ def orphan_cleanup():
     Delete all orphan Content and Artifact records.
     This task removes Artifact files from the filesystem as well.
     """
-    # Content cleanup
-    content = Content.objects.exclude(
-        pk__in=RepositoryContent.objects.values_list("content_id", flat=True)
+    content = Content.objects.filter(version_memberships__isnull=True).exclude(
+        pulp_type=PublishedMetadata.get_pulp_type()
     )
-    content = content.exclude(pulp_type="core.{}".format(PublishedMetadata.TYPE))
+
+    content_count = content.count()
     progress_bar = ProgressReport(
         message="Clean up orphan Content",
-        total=content.count(),
+        total=content_count,
         code="clean-up.content",
         done=0,
         state="running",
     )
     progress_bar.save()
-    content.delete()
-    progress_bar.done = progress_bar.total
+
+    # delete the content
+    for c in queryset_iterator(content):
+        progress_bar.increase_by(c.count())
+        c.delete()
+
     progress_bar.state = "completed"
     progress_bar.save()
 
-    # Artifact cleanup
-    artifacts = Artifact.objects.exclude(
-        pk__in=ContentArtifact.objects.values_list("artifact_id", flat=True)
-    )
+    # delete the artifacts that don't belong to any content
+    artifacts = Artifact.objects.filter(content_memberships__isnull=True)
+
     progress_bar = ProgressReport(
         message="Clean up orphan Artifacts",
         total=artifacts.count(),
@@ -43,10 +67,18 @@ def orphan_cleanup():
         state="running",
     )
     progress_bar.save()
-    for artifact in artifacts:
+
+    counter = 0
+    interval = 100
+    for artifact in artifacts.iterator():
         # we need to manually call delete() because it cleans up the file on the filesystem
         artifact.delete()
-        progress_bar.increment()
+        progress_bar.done += 1
+        counter += 1
+
+        if counter >= interval:
+            progress_bar.save()
+            counter = 0
 
     progress_bar.state = "completed"
     progress_bar.save()
