dbd41e5e7da80ea15346dc71b40f2a5e97a3ba9b
Tatiana Tereshchenko <ttereshc@redhat.com>
2020-04-15 18:19:48 +0200

    Bump version to 3.3.0
    [noissue]

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ requirements = [
 
 setup(
     name='pulpcore',
-    version='3.3.0.dev',
+    version='3.3.0',
     description='Pulp Django Application and Related Modules',
     long_description=long_description,
     long_description_content_type="text/markdown",
