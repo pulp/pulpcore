b342bbe7e1bfff0db5ab146c1c56c7a2d08a43af
Daniel Alley <dalley@redhat.com>
2020-09-29 15:10:08 -0400

    Expose pulpcore_version to plugin writers
    Give plugin writers a way to access the pulpcore version.
    This is possiblyuseful in case they want to support more than one
    version of the plugin API at the same time.
    
    closes: #7624
    https://pulp.plan.io/issues/7624

--- pulpcore/plugin/__init__.py
+++ pulpcore/plugin/__init__.py
@@ -1,2 +1,5 @@
 # plugins declare that they are a pulp plugin by subclassing PulpPluginAppConfig
 from pulpcore.app.apps import PulpPluginAppConfig  # noqa
+
+# allow plugins to access the pulpcore version
+from pulpcore import __version__ as pulpcore_version  # noqa
