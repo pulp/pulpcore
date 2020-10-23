32f8eb95cc2f92fa7f10c372b8f3dc5c31b61b83
Bruno Rocha <rochacbruno@gmail.com>
2020-10-23 15:47:11 -0300

    Expose ProgressReportSerializer on pulpcore.plugin
    Currently it is only possible to import ProgressReportSerializer through
    `pulpcore.app.serializers` and it is useful to have it on
    `pulpcore.plugin.serializers`
    
    Related PR: https://github.com/ansible/galaxy_ng/pull/526
    
    closes #7759

--- pulpcore/plugin/serializers/__init__.py
+++ pulpcore/plugin/serializers/__init__.py
@@ -16,6 +16,7 @@ from pulpcore.app.serializers import (  # noqa
     MultipleArtifactContentSerializer,
     NestedRelatedField,
     NoArtifactContentSerializer,
+    ProgressReportSerializer,
     PublicationDistributionSerializer,
     PublicationExportSerializer,
     PublicationSerializer,
