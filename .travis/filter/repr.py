345a4870118c936cce9705eaa96ca3a0bd8335ca
Matthias Dellweg <mdellweg@redhat.com>
2020-10-30 13:34:01 +0100

    Update .travis dir
    [noissue]

--- .travis/filter/repr.py
+++ .travis/filter/repr.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import, division, print_function
+from packaging.version import parse as parse_version
 
 __metaclass__ = type
 
@@ -14,6 +15,10 @@ def _repr_filter(value):
     return repr(value)
 
 
+def _canonical_semver_filter(value):
+    return str(parse_version(value))
+
+
 # ---- Ansible filters ----
 class FilterModule(object):
     """Repr filter."""
@@ -22,4 +27,5 @@ class FilterModule(object):
         """Filter associations."""
         return {
             "repr": _repr_filter,
+            "canonical_semver": _canonical_semver_filter,
         }
