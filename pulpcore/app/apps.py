9be7ce03acb98b95ed1cfcbbd2a5d4bf7ff130da
David Davis <daviddavis@redhat.com>
2020-12-07 12:34:43 -0500

    Bump to 3.10.0.dev
    [noissue]

--- pulpcore/app/apps.py
+++ pulpcore/app/apps.py
@@ -183,7 +183,7 @@ class PulpAppConfig(PulpPluginAppConfig):
     label = "core"
 
     # The version of this app
-    version = "3.9.0"
+    version = "3.10.0.dev"
 
 
 def _populate_access_policies(sender, **kwargs):
