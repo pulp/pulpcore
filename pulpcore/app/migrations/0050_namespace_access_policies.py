cf97743596faf223d5e0f6e2950c1c1ee5aa0569
Brian Bouterse <bmbouter@redhat.com>
2021-02-03 10:40:37 -0500

    Fixes migration imports to use apps.get_model
    In various places in the migrations Models are imported directly and then
    used. This is a hazard for future changes because when the model
    definition changes this code can break.
    
    This PR switches data migrations to use apps.get_model(...) intead. It
    also fixes a syntax migration I noticed on the downmigrate in migration
    50.
    
    closes #8203

--- pulpcore/app/migrations/0050_namespace_access_policies.py
+++ pulpcore/app/migrations/0050_namespace_access_policies.py
@@ -2,17 +2,17 @@
 
 from django.db import migrations
 
-from pulpcore.app.models import AccessPolicy
-
 
 def namespace_access_policies_up(apps, schema_editor):
+    AccessPolicy = apps.get_model('core', 'AccessPolicy')
     task_policy = AccessPolicy.objects.get(viewset_name="TaskViewSet")
     task_policy.viewset_name = "tasks"
     task_policy.save()
 
 
 def namespace_access_policies_down(apps, schema_editor):
-    AccessPolicy.objects.get(viewset_name="tasks").delete()
+    AccessPolicy = apps.get_model('core', 'AccessPolicy')
+    task_policy = AccessPolicy.objects.get(viewset_name="tasks").delete()
     task_policy.viewset_name = "TaskViewSet"
     task_policy.save()
 
