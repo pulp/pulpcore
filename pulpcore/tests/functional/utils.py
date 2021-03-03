cde11ccf043c193f6c4ae5c5e3a14ead46a66951
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2021-03-03 10:16:43 -0300

    Update require_pulp_plugins
    [noissue]

--- pulpcore/tests/functional/utils.py
+++ pulpcore/tests/functional/utils.py
@@ -11,7 +11,7 @@ from pulpcore.client.pulpcore import ApiClient
 def set_up_module():
     """Skip tests Pulp 3 isn't under test or if pulpcore isn't installed."""
     require_pulp_3(SkipTest)
-    require_pulp_plugins({"pulpcore"}, SkipTest)
+    require_pulp_plugins({"core"}, SkipTest)
 
 
 skip_if = partial(selectors.skip_if, exc=SkipTest)  # pylint:disable=invalid-name
