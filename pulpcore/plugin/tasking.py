da89c10255bf73e03c34fb2e33e99a365c8dc40f
Matthias Dellweg <mdellweg@redhat.com>
2021-03-19 19:47:29 +0100

    Remove deprecated WorkingDirectory from tasking
    fixes #8354

--- pulpcore/plugin/tasking.py
+++ pulpcore/plugin/tasking.py
@@ -2,9 +2,6 @@
 from pulpcore.tasking.tasks import enqueue_with_reservation  # noqa
 from pulpcore.tasking.tasks import dispatch  # noqa
 
-# Support plugins working with the working directory.
-from pulpcore.tasking.storage import WorkingDirectory  # noqa
-
 # Plugin export tasks
 from pulpcore.app.tasks import fs_publication_export, fs_repo_version_export  # noqa
 from pulpcore.app.tasks.repository import add_and_remove  # noqa
