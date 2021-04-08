4bda1a562b16248b18d252b52f296ec402052661
Alexander Saprykin <cutwatercore@gmail.com>
2021-04-08 13:24:39 +0200

    Fix missing REDIS_SSL parameter in rqconfig.py
    closes: #8525
    https://pulp.plan.io/issues/8525

--- pulpcore/rqconfig.py
+++ pulpcore/rqconfig.py
@@ -2,6 +2,14 @@ import sys
 
 from pulpcore.app.settings import settings
 
-rq_settings = ["REDIS_URL", "REDIS_HOST", "REDIS_PORT", "REDIS_DB", "REDIS_PASSWORD", "SENTINEL"]
+rq_settings = [
+    "REDIS_URL",
+    "REDIS_HOST",
+    "REDIS_PORT",
+    "REDIS_DB",
+    "REDIS_PASSWORD",
+    "REDIS_SSL",
+    "SENTINEL",
+]
 
 settings.populate_obj(sys.modules[__name__], keys=rq_settings)
