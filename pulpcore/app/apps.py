52027039903d8768ad486843d45bc8be7d2da301
Matthias Dellweg <mdellweg@redhat.com>
2021-04-08 18:29:04 +0200

    Bump version to 3.12.1.dev
    [noissue]

--- pulpcore/app/apps.py
+++ pulpcore/app/apps.py
@@ -183,7 +183,7 @@ class PulpAppConfig(PulpPluginAppConfig):
     label = "core"
 
     # The version of this app
-    version = "3.12.0"
+    version = "3.12.1.dev"
 
     def ready(self):
         super().ready()
