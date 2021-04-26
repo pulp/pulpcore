06ec94f6de0c972e8ce730f1542f12fb38ac6958
Matthias Dellweg <mdellweg@redhat.com>
2021-04-26 11:11:59 +0200

    Reformat with black 21.4b0
    [noissue]

--- pulpcore/openapi/__init__.py
+++ pulpcore/openapi/__init__.py
@@ -352,7 +352,7 @@ class PulpSchemaGenerator(SchemaGenerator):
         return path
 
     def parse(self, input_request, public):
-        """ Iterate endpoints generating per method path operations. """
+        """Iterate endpoints generating per method path operations."""
         result = {}
         self._initialise_endpoints()
         endpoints = self._get_paths_and_endpoints(None if public else input_request)
@@ -449,7 +449,7 @@ class PulpSchemaGenerator(SchemaGenerator):
         return result
 
     def get_schema(self, request=None, public=False):
-        """ Generate a OpenAPI schema. """
+        """Generate a OpenAPI schema."""
         reset_generator_stats()
         result = build_root_object(
             paths=self.parse(request, public),
