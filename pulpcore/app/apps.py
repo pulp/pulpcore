26b4e0a728d147837baeaa1dfec6d1f21911c8a4
Daniel Alley <dalley@redhat.com>
2021-05-25 15:46:54 -0400

    Release 3.13.0
    Redmine Query: https://pulp.plan.io/issues?set_filter=1&status_id=*&issue_id=8691,8396,8633,8723,7747,8578,8603,8541,7776,8584,8625,8595,7019,8386,8477,8609,8525,4832,8628,6990,8524,8760,6068,8792,7715,7316,8368,8636,8689,8635,8668,8784,8597,8721,6703,8354,8761,8793,8656,8497,8711,6235,8550
    Redmine Milestone: https://pulp.plan.io/versions/188.json
    [noissue]

--- pulpcore/app/apps.py
+++ pulpcore/app/apps.py
@@ -185,7 +185,7 @@ class PulpAppConfig(PulpPluginAppConfig):
     label = "core"
 
     # The version of this app
-    version = "3.13.0.dev"
+    version = "3.13.0"
 
     def ready(self):
         super().ready()
