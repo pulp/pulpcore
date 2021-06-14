fa96cdc41cafee827e657411a7ad0270e20c38b9
Brian Bouterse <bmbouter@redhat.com>
2021-06-14 14:48:41 -0400

    Revert "Add orphan cleanup stop gap in pulpcore-worker"
    This reverts commit 96aa50424a6f1f8e2483e2341d9847cbc6b6421c.
    
    [noissue]

--- pulpcore/tasking/pulpcore_worker.py
+++ pulpcore/tasking/pulpcore_worker.py
@@ -171,16 +171,7 @@ class NewPulpWorker:
         with WorkerDirectory(self.name):
             while True:
                 for task in self.iter_tasks():
-                    try:
-                        # Workaround to block all other workers
-                        if task.name == "pulpcore.app.tasks.orphan.orphan_cleanup":
-                            suffix = ""
-                        else:
-                            suffix = "_shared"
-                        self.cursor.execute(f"SELECT pg_advisory_lock{suffix}(1234)")
-                        self.supervise_task(task)
-                    finally:
-                        self.cursor.execute(f"SELECT pg_advisory_unlock{suffix}(1234)")
+                    self.supervise_task(task)
                 self.sleep()
 
 
