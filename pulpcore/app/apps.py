d096d62bf29c8b09c9af7dd1d13345475dbfc7d4
pulpbot <pulp-infra@redhat.com>
2021-08-26 09:54:45 +0000

    Bump minor version [noissue]

--- pulpcore/app/apps.py
+++ pulpcore/app/apps.py
@@ -186,7 +186,7 @@ class PulpAppConfig(PulpPluginAppConfig):
     label = "core"
 
     # The version of this app
-    version = "3.15.0.dev"
+    version = "3.16.0.dev"
 
     def ready(self):
         super().ready()
