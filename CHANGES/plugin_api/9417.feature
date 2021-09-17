20f6520a8d3c6e6ff7b05d02bcbbe3e4d02a53dd
Ina Panova <ipanova@redhat.com>
2021-09-17 13:09:31 +0200

    Ensured that with the removal of ACS it's hidden repositories are removed as well.
    closes #9417
    closes #9374

---
+++ CHANGES/plugin_api/9417.feature
@@ -0,0 +1,2 @@
+Added the ``pulpcore.plugin.tasking.general_multi_delete`` that deletes a list of model instances
+in a transaction.
--- pulpcore/plugin/tasking.py
+++ pulpcore/plugin/tasking.py
@@ -1,6 +1,9 @@
 # Support plugins dispatching tasks
 from pulpcore.tasking.tasks import dispatch  # noqa
 
-# Plugin export tasks
-from pulpcore.app.tasks import fs_publication_export, fs_repo_version_export  # noqa
+from pulpcore.app.tasks import (  # noqa
+    general_multi_delete,
+    fs_publication_export,
+    fs_repo_version_export,
+)
 from pulpcore.app.tasks.repository import add_and_remove  # noqa
