b304e8f38afddb12a77d1774d1ed8435318306e2
David Davis <daviddavis@redhat.com>
2021-09-21 15:39:58 -0400

    Expose tasks in the plugin api
    fixes #9418

---
+++ CHANGES/plugin_api/9418.feature
@@ -0,0 +1,2 @@
+Exposed tasks ``general_create``, ``general_create_from_temp_file``, ``general_delete``,
+``general_update``, ``orphan_cleanup``, and ``reclaim_space`` in the plugin api.
--- pulpcore/plugin/tasking.py
+++ pulpcore/plugin/tasking.py
@@ -2,8 +2,14 @@
 from pulpcore.tasking.tasks import dispatch  # noqa
 
 from pulpcore.app.tasks import (  # noqa
-    general_multi_delete,
     fs_publication_export,
     fs_repo_version_export,
+    general_create,
+    general_create_from_temp_file,
+    general_delete,
+    general_multi_delete,
+    general_update,
+    orphan_cleanup,
+    reclaim_space,
 )
 from pulpcore.app.tasks.repository import add_and_remove  # noqa
