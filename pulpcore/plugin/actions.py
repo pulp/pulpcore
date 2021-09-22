1f7ec2409617200ee7e913cfb21046efce9b1529
Daniel Alley <dalley@redhat.com>
2021-09-22 09:17:40 -0400

    Do bulk_touch instead of touching one by one
    backports #9266.
    closes: #9401
    
    (cherry picked from commit 4023fce)

--- pulpcore/plugin/actions.py
+++ pulpcore/plugin/actions.py
@@ -1,6 +1,7 @@
-from django.db import DatabaseError
+from gettext import gettext as _
 from drf_spectacular.utils import extend_schema
 from rest_framework.decorators import action
+from rest_framework.serializers import ValidationError
 
 from pulpcore.app import tasks
 from pulpcore.app.models import Content, RepositoryVersion
@@ -9,6 +10,7 @@ from pulpcore.app.serializers import (
     AsyncOperationResponseSerializer,
     RepositoryAddRemoveContentSerializer,
 )
+from pulpcore.app.viewsets import NamedModelViewSet
 from pulpcore.tasking.tasks import dispatch
 
 
@@ -26,8 +28,9 @@ class ModifyRepositoryActionMixin:
         """
         Queues a task that creates a new RepositoryVersion by adding and removing content units
         """
-        add_content_units = []
-        remove_content_units = []
+        add_content_units = {}
+        remove_content_units = {}
+
         repository = self.get_object()
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -39,22 +42,26 @@ class ModifyRepositoryActionMixin:
 
         if "add_content_units" in request.data:
             for url in request.data["add_content_units"]:
-                content = self.get_resource(url, Content)
-                try:
-                    content.touch()
-                except DatabaseError:
-                    # content has since been removed. call get_url to raise an exception.
-                    content = self.get_resource(url, Content)
-                add_content_units.append(content.pk)
+                add_content_units[NamedModelViewSet.extract_pk(url)] = url
+
+            content_units_pks = set(add_content_units.keys())
+            existing_content_units = Content.objects.filter(pk__in=content_units_pks)
+            existing_content_units.touch()
+
+            self.verify_content_units(existing_content_units, add_content_units)
+
+            add_content_units = list(add_content_units.keys())
 
         if "remove_content_units" in request.data:
-            for url in request.data["remove_content_units"]:
-                if url == "*":
-                    remove_content_units = [url]
-                    break
-                else:
-                    content = self.get_resource(url, Content)
-                    remove_content_units.append(content.pk)
+            if "*" in request.data["remove_content_units"]:
+                remove_content_units = ["*"]
+            else:
+                for url in request.data["remove_content_units"]:
+                    remove_content_units[NamedModelViewSet.extract_pk(url)] = url
+                content_units_pks = set(remove_content_units.keys())
+                existing_content_units = Content.objects.filter(pk__in=content_units_pks)
+                self.verify_content_units(existing_content_units, remove_content_units)
+                remove_content_units = list(remove_content_units.keys())
 
         task = dispatch(
             tasks.repository.add_and_remove,
@@ -67,3 +74,15 @@ class ModifyRepositoryActionMixin:
             },
         )
         return OperationPostponedResponse(task, request)
+
+    def verify_content_units(self, content_units, all_content_units):
+        """Verify referenced content units."""
+        existing_content_units_pks = content_units.values_list("pk", flat=True)
+        existing_content_units_pks = {str(pk) for pk in existing_content_units_pks}
+
+        missing_pks = set(all_content_units.keys()) - existing_content_units_pks
+        if missing_pks:
+            missing_hrefs = [all_content_units[pk] for pk in missing_pks]
+            raise ValidationError(
+                _("Could not find the following content units: {}").format(missing_hrefs)
+            )
