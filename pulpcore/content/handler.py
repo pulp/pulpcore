bcfc5ac5f3d99c705099be2de8070a6cd28f63ab
Dennis Kliban <dkliban@redhat.com>
2021-10-29 12:50:50 -0400

    Handles closed db connections
    When the authentication middleware was added in pulpcore 3.15, it became the first place
    in the content app that made an attempt to use the database. As a result, it is a convinient
    place to handle InterfaceError and  OperationalError which are raised when the database
    connection has been closed. When this occurs, Handler._reset_db_connection() is called to
    clean up the database connection and the middleware tries to use the database again.
    
    If the database connection is closed later in the handling of the request by the content app,
    the user will still get a 500 error. However, the next request will be handled properly.
    
    This patch also adds a call to Handler._reset_db_connection() inside the heartbeat method.
    
    backports #9276
    
    fixes: #9598
    https://pulp.plan.io/issues/9598
    
    (cherry picked from commit 3faa649ddb0737c23d1e309a8c38ecb41804cebe)

--- pulpcore/content/handler.py
+++ pulpcore/content/handler.py
@@ -121,7 +121,6 @@ class Handler:
         Returns:
             :class:`aiohttp.web.HTTPOk`: The response back to the client.
         """
-        self._reset_db_connection()
 
         def get_base_paths_blocking():
             if self.distribution_model is None:
@@ -200,8 +199,6 @@ class Handler:
             :class:`aiohttp.web.StreamResponse` or :class:`aiohttp.web.FileResponse`: The response
                 back to the client.
         """
-        self._reset_db_connection()
-
         path = request.match_info["path"]
         return await self._match_and_stream(path, request)
 
@@ -240,8 +237,6 @@ class Handler:
         Raises:
             PathNotResolved: when not matched.
         """
-        cls._reset_db_connection()
-
         base_paths = cls._base_paths(path)
         if cls.distribution_model is None:
             try:
