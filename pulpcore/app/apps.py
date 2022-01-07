955750c12e31d9c2a279f58dffb306f72808e9a6
pulpbot <pulp-infra@redhat.com>
2022-01-07 13:26:36 +0000

    Release 3.14.10
    Redmine Query: https://pulp.plan.io/issues?set_filter=1&status_id=*&issue_id=9662
    Redmine Milestone: https://pulp.plan.io/versions/346.json
    [noissue]

--- pulpcore/app/apps.py
+++ pulpcore/app/apps.py
@@ -185,7 +185,7 @@ class PulpAppConfig(PulpPluginAppConfig):
     label = "core"
 
     # The version of this app
-    version = "3.14.10.dev"
+    version = "3.14.10"
 
     def ready(self):
         super().ready()
