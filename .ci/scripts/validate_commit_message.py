7de439858077f867ebfba70880969f29ac920caf
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2022-01-17 19:49:37 +0000

    Use github issues instead of redmine
    [noissue]

--- .ci/scripts/validate_commit_message.py
+++ .ci/scripts/validate_commit_message.py
@@ -11,38 +11,33 @@ import sys
 from pathlib import Path
 
 
-import requests
+import os
+import warnings
+from github import Github
 
 
 NO_ISSUE = "[noissue]"
 CHANGELOG_EXTS = [".feature", ".bugfix", ".doc", ".removal", ".misc", ".deprecation"]
 
 
-KEYWORDS = ["fixes", "closes", "re", "ref"]
-STATUSES = ["NEW", "ASSIGNED", "POST", "MODIFIED"]
-REDMINE_URL = "https://pulp.plan.io"
+KEYWORDS = ["fixes", "closes"]
 
 sha = sys.argv[1]
-project = "pulp"
 message = subprocess.check_output(["git", "log", "--format=%B", "-n 1", sha]).decode("utf-8")
+g = Github(os.environ.get("GITHUB_TOKEN"))
+repo = g.get_repo("pulp/pulpcore")
 
 
 def __check_status(issue):
-    response = requests.get(f"{REDMINE_URL}/issues/{issue}.json")
-    response.raise_for_status()
-    bug_json = response.json()
-    status = bug_json["issue"]["status"]["name"]
-    if status not in STATUSES:
-        sys.exit(
-            "Error: issue #{issue} has invalid status of {status}. Status must be one of "
-            "{statuses}.".format(issue=issue, status=status, statuses=", ".join(STATUSES))
+    gi = repo.get_issue(int(issue))
+    if gi.pull_request:
+        sys.exit(f"Error: issue #{issue} is a pull request.")
+    if gi.closed_at and "cherry picked from commit" not in message:
+        warnings.warn(
+            "When backporting, use the -x flag to append a line that says "
+            "'(cherry picked from commit ...)' to the original commit message."
         )
-
-    if project:
-        project_id = bug_json["issue"]["project"]["id"]
-        project_json = requests.get(f"{REDMINE_URL}/projects/{project_id}.json").json()
-        if project_json["project"]["identifier"] != project:
-            sys.exit(f"Error: issue {issue} is not in the {project} project.")
+        sys.exit(f"Error: issue #{issue} is closed.")
 
 
 def __check_changelog(issue):
