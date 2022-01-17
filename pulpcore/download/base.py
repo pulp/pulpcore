7ec9d524da88234a2429dcd4b9c4a84eaf33c09a
Matthias Dellweg <mdellweg@redhat.com>
2022-01-17 13:03:45 +0100

    Fix retry logic of partially downloaded files
    fixes #2078
    
    (cherry picked from commit 344f616c0d55df2c3e7d6e84bc12008942f1e8ae)

--- pulpcore/download/base.py
+++ pulpcore/download/base.py
@@ -162,6 +162,7 @@ class BaseDownloader:
         self._writer.flush()
         os.fsync(self._writer.fileno())
         self._writer.close()
+        self._writer = None
         self.validate_digests()
         self.validate_size()
 
