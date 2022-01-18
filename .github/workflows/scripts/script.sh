85d366162602f28bab5562d75cdeb87f5d04cb20
Brian Bouterse <bmbouter@redhat.com>
2022-01-18 14:32:54 -0500

    Enable parallel test running from plugin_template
    This is a manual application of the pending plugin_template PR linked
    below.
    
    https://github.com/pulp/plugin_template/pull/547
    
    [noissue]

--- .github/workflows/scripts/script.sh
+++ .github/workflows/scripts/script.sh
@@ -177,7 +177,10 @@ fi
 if [ -f $FUNC_TEST_SCRIPT ]; then
   source $FUNC_TEST_SCRIPT
 else
-    pytest -v -r sx --color=yes --pyargs pulpcore.tests.functional
+
+    pytest -v -r sx --color=yes --suppress-no-test-exit-code --pyargs pulpcore.tests.functional -m parallel -n 8
+    pytest -v -r sx --color=yes --pyargs pulpcore.tests.functional -m "not parallel"
+
 fi
 export PULP_FIXTURES_URL="http://pulp-fixtures:8080"
 pushd ../pulp-cli
