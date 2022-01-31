66634bb5212bb1cc4eb4d88800b9f8e4d5163662
Brian McLaughlin <bmclaugh@redhat.com>
2022-01-31 18:46:40 -0500

    Fixes circular import for plugins with custom user model fixes #2185

---
+++ pulpcore/plugin/models/role.py
@@ -0,0 +1,8 @@
+# Models are exposed selectively in the versioned plugin API.
+# Any models defined in the pulpcore.plugin namespace should probably be proxy models.
+
+from pulpcore.app.models.role import (  # noqa
+    GroupRole,
+    Role,
+    UserRole,
+)
