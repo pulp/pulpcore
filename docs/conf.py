297af8dca4a4ef71a6c339b25869a453f4c7ab59
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2022-02-04 16:04:55 +0000

    Make docs easier to run locally
    [noissue]

--- docs/conf.py
+++ docs/conf.py
@@ -78,7 +78,7 @@ release = "3.18.0.dev"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build', 'pulpdocs']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
