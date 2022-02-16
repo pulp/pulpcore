bbf87201f31bf3af9d25695b4797379f36a020b9
Matthias Dellweg <mdellweg@redhat.com>
2022-02-16 12:32:55 +0100

    Deprecate the API_ROOT constant
    It was turned into a setting.
    
    re #2148
    [noissue]

--- pulpcore/plugin/constants.py
+++ pulpcore/plugin/constants.py
@@ -1,9 +1,20 @@
 from pulpcore.constants import (  # noqa
     ALL_KNOWN_CONTENT_CHECKSUMS,
-    API_ROOT,
     SYNC_MODES,
     SYNC_CHOICES,
     TASK_STATES,
     TASK_CHOICES,
     TASK_FINAL_STATES,
 )
+
+
+@property
+def API_ROOT():
+    from django.conf import settings
+    from pulpcore.app.loggers import deprecation_logger
+
+    deprecation_logger.warn(
+        "The API_ROOT constant has been deprecated and turned into a setting. Please use "
+        "`settings.API_ROOT` instead. This symbol will be deleted with pulpcore 3.20."
+    )
+    return settings.API_ROOT
