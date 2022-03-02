8bab542d2c10deb64bc32d61e29b0f051c5d325f
Grant Gainey <ggainey@redhat.com>
2022-03-02 15:49:34 -0500

    Fix new-task-system migration to clean up task and taskreservedresource.
    fixes #2101
    [nocoverage]
    
    (cherry picked from commit 8e72f73806e6ad3f622007883fd3180bdbdd2d60)

--- pulpcore/app/migrations/0064_add_new_style_task_columns.py
+++ pulpcore/app/migrations/0064_add_new_style_task_columns.py
@@ -4,16 +4,34 @@ import django.contrib.postgres.fields
 import django.contrib.postgres.fields.jsonb
 from django.db import migrations, models
 
+TASK_BATCH_SIZE = 1000
+
 
 def copy_reserved_resources_record(apps, schema_editor):
     Task = apps.get_model('core', 'Task')
-    for task in Task.objects.iterator():
+
+    # Update _reserved_resource_record for all tasks, 1000 tasks at a time.
+    # When we hit 1K tasks, go to the db for the batch.
+    # Make sure to update the final batch!
+    tasks = []
+    for task in Task.objects.iterator(chunk_size=TASK_BATCH_SIZE):
         task._reserved_resources_record = list(task.reserved_resources_record.values_list('resource', flat=True))
-        task.save()
+        tasks.append(task)
+        if len(tasks) == TASK_BATCH_SIZE:
+            Task.objects.bulk_update(tasks, ["_reserved_resources_record"])
+            tasks.clear()
+
+    # Update last set of tasks
+    if len(tasks) > 0:
+        Task.objects.bulk_update(tasks, ["_reserved_resources_record"])
 
 
-def noop(apps, schema_editor):
-    pass
+def purge_reservedresources(apps, schema_editor):
+    TaskReservedResource = apps.get_model('core', 'TaskReservedResource')
+    TaskReservedResource.objects.all().delete()
+
+    ReservedResource = apps.get_model('core', 'ReservedResource')
+    ReservedResource.objects.all().delete()
 
 
 class Migration(migrations.Migration):
@@ -23,6 +41,12 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        # Purge any ReservedResource entries - if there are any, they're orphans
+        migrations.RunPython(
+            code=purge_reservedresources,
+            reverse_code=migrations.RunPython.noop,
+        ),
+        # Update entities for the new task-system
         migrations.AddField(
             model_name='task',
             name='args',
@@ -59,7 +83,7 @@ class Migration(migrations.Migration):
         ),
         migrations.RunPython(
             code=copy_reserved_resources_record,
-            reverse_code=noop,
+            reverse_code=migrations.RunPython.noop,
         ),
         migrations.RemoveField(
             model_name='taskreservedresourcerecord',
@@ -80,4 +104,5 @@ class Migration(migrations.Migration):
             old_name='_reserved_resources_record',
             new_name='reserved_resources_record',
         ),
+
     ]
