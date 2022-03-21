799c9c639f9424ad6f7a4c0c7ce779c29300cecd
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2022-03-21 11:28:08 +0000

    Update 3.15 CI
    [noissue]

--- .github/workflows/scripts/publish_docs.sh
+++ .github/workflows/scripts/publish_docs.sh
@@ -31,3 +31,43 @@ eval "$(ssh-agent -s)" #start the ssh agent
 ssh-add ~/.ssh/pulp-infra
 
 python3 .github/workflows/scripts/docs-publisher.py --build-type $1 --branch $2
+
+if [[ "$GITHUB_WORKFLOW" == "Pulpcore changelog update" ]]; then
+  # Do not build bindings docs on changelog update
+  exit
+fi
+
+# Building python bindings
+export PULP_URL="${PULP_URL:-https://pulp}"
+VERSION=$(http $PULP_URL/pulp/api/v3/status/ | jq --arg plugin core --arg legacy_plugin pulpcore -r '.versions[] | select(.component == $plugin or .component == $legacy_plugin) | .version')
+cd ../pulp-openapi-generator
+rm -rf pulpcore-client
+./generate.sh pulpcore python $VERSION
+cd pulpcore-client
+
+# Adding mkdocs
+find ./docs/* -exec sed -i 's/README//g' {} \;
+cp README.md docs/index.md
+sed -i 's/docs\///g' docs/index.md
+find ./docs/* -exec sed -i 's/\.md//g' {} \;
+cat >> mkdocs.yml << DOCSYAML
+---
+site_name: Pulpcore Client
+site_description: Pulpcore bindings
+site_author: Pulp Team
+site_url: https://docs.pulpproject.org/pulpcore_client/
+repo_name: pulp/pulpcore
+repo_url: https://github.com/pulp/pulpcore
+theme: readthedocs
+DOCSYAML
+
+pip install mkdocs pymdown-extensions
+
+# Building the bindings docs
+mkdocs build
+
+# publish to docs.pulpproject.org/pulpcore_client
+rsync -avzh site/ doc_builder_pulpcore@docs.pulpproject.org:/var/www/docs.pulpproject.org/pulpcore_client/
+
+# publish to docs.pulpproject.org/pulpcore_client/en/{release}
+rsync -avzh site/ doc_builder_pulpcore@docs.pulpproject.org:/var/www/docs.pulpproject.org/pulpcore_client/en/"$1"
