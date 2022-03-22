e302891263d62fadd0fefe156db8addaa2fc1bcf
Brian Bouterse <bmbouter@redhat.com>
2022-03-22 15:38:04 -0400

    Fixes RemoteArtifact duplicate key error
    The Django Prefetch caching is providing older data previously fetched,
    so we need to invaidate the caches when prefetching.
    
    closes #2381
    
    (cherry picked from commit 022ce83040f24d8a5fb74a6c6c3325b07691dc36)

--- pulpcore/plugin/stages/artifact_stages.py
+++ pulpcore/plugin/stages/artifact_stages.py
@@ -293,6 +293,11 @@ class RemoteArtifactSaver(Stage):
         """
         remotes_present = set()
         for d_content in batch:
+            # https://code.djangoproject.com/ticket/33596
+            # If the content was pre-fetched previously, remove that cached data, which could be out
+            # of date.
+            if hasattr(d_content.content, "_remote_artifact_saver_cas"):
+                delattr(d_content.content, "_remote_artifact_saver_cas")
             for d_artifact in d_content.d_artifacts:
                 if d_artifact.remote:
                     remotes_present.add(d_artifact.remote)
