6745ad1b04eadeea9b66a65c7c86f1d6caf41576
Brian Bouterse <bmbouter@redhat.com>
2022-03-22 12:24:03 -0400

    Removes the Django Admin UI Site
    This was offered with tech-preview, but now with the RBAC, User, and
    Group APIs we no longer need it.
    
    closes #2374

--- pulpcore/app/settings.py
+++ pulpcore/app/settings.py
@@ -37,8 +37,6 @@ ALLOWED_HOSTS = ["*"]
 DEPLOY_ROOT = Path("/var/lib/pulp")
 MEDIA_ROOT = str(DEPLOY_ROOT / "media")  # Django 3.1 adds support for pathlib.Path
 
-ADMIN_SITE_URL = "admin/"
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
@@ -67,7 +65,6 @@ API_ROOT = "/pulp/"
 
 INSTALLED_APPS = [
     # django stuff
-    "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
