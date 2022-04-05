e644e5772c64fef435295cfaf8ba0645dc75f877
Ľuboš Mjachky <lmjachky@redhat.com>
2022-04-05 20:41:09 +0200

    Fix the note about a default value
    [noissue]

--- pulpcore/app/serializers/orphans.py
+++ pulpcore/app/serializers/orphans.py
@@ -19,8 +19,8 @@ class OrphansCleanupSerializer(serializers.Serializer, ValidateFieldsMixin):
             "The time in minutes for how long Pulp will hold orphan Content and Artifacts before "
             "they become candidates for deletion by this orphan cleanup task. This should ideally "
             "be longer than your longest running task otherwise any content created during that "
-            "task could be cleaned up before the task finishes. If not specified, default is used "
-            "from settings which is 1440 minutes (24 hours)"
+            "task could be cleaned up before the task finishes. If not specified, a default value "
+            "is taken from the setting ORPHAN_PROTECTION_TIME."
         ),
         allow_null=True,
         required=False,
