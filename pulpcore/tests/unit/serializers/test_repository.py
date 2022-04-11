ba4832a889947f5e2253b75442a18a7f333ec48e
Dennis Kliban <dkliban@redhat.com>
2022-04-11 16:25:05 -0400

    Switches unit test runner to pytest
    [noissue]

--- pulpcore/tests/unit/serializers/test_repository.py
+++ pulpcore/tests/unit/serializers/test_repository.py
@@ -4,9 +4,7 @@ from types import SimpleNamespace
 import mock
 from rest_framework import serializers
 
-from pulpcore.app.models import Distribution
 from pulpcore.app.serializers import (
-    DistributionSerializer,
     PublicationSerializer,
     RemoteSerializer,
 )
@@ -14,38 +12,6 @@ from pulpcore.app.serializers import (
 
 class TestRemoteSerializer(TestCase):
     minimal_data = {"name": "test", "url": "http://whatever"}
-
-    def test_minimal_data(self):
-        data = {}
-        data.update(self.minimal_data)
-        serializer = RemoteSerializer(data=data)
-        serializer.is_valid(raise_exception=True)
-
-    def test_validate_proxy(self):
-        data = {"proxy_url": "http://whatever"}
-        data.update(self.minimal_data)
-        serializer = RemoteSerializer(data=data)
-        serializer.is_valid(raise_exception=True)
-
-    def test_validate_proxy_invalid(self):
-        data = {"proxy_url": "http://user:pass@whatever"}
-        data.update(self.minimal_data)
-        serializer = RemoteSerializer(data=data)
-        with self.assertRaises(serializers.ValidationError):
-            serializer.is_valid(raise_exception=True)
-
-    def test_validate_proxy_creds(self):
-        data = {"proxy_url": "http://whatever", "proxy_username": "user", "proxy_password": "pass"}
-        data.update(self.minimal_data)
-        serializer = RemoteSerializer(data=data)
-        serializer.is_valid(raise_exception=True)
-
-    def test_validate_proxy_creds_invalid(self):
-        data = {"proxy_url": "http://whatever", "proxy_username": "user"}
-        data.update(self.minimal_data)
-        serializer = RemoteSerializer(data=data)
-        with self.assertRaises(serializers.ValidationError):
-            serializer.is_valid(raise_exception=True)
 
     def test_validate_proxy_creds_update(self):
         Remote = SimpleNamespace(
@@ -115,58 +81,3 @@ class TestPublicationSerializer(TestCase):
         serializer = PublicationSerializer(data=data)
         with self.assertRaises(serializers.ValidationError):
             serializer.validate(data)
-
-
-class TestDistributionPath(TestCase):
-    def test_overlap(self):
-        Distribution.objects.create(base_path="foo/bar", name="foobar")
-        overlap_errors = {"base_path": ["Overlaps with existing distribution 'foobar'"]}
-
-        # test that the new distribution cannot be nested in an existing path
-        data = {"name": "foobarbaz", "base_path": "foo/bar/baz"}
-        serializer = DistributionSerializer(data=data)
-        self.assertFalse(serializer.is_valid())
-        self.assertDictEqual(overlap_errors, serializer.errors)
-
-        # test that the new distribution cannot nest an existing path
-        data = {"name": "foo", "base_path": "foo"}
-        serializer = DistributionSerializer(data=data)
-        self.assertFalse(serializer.is_valid())
-        self.assertDictEqual(overlap_errors, serializer.errors)
-
-    def test_no_overlap(self):
-        Distribution.objects.create(base_path="fu/bar", name="fubar")
-
-        # different path
-        data = {"name": "fufu", "base_path": "fubar"}
-        serializer = DistributionSerializer(data=data)
-        self.assertTrue(serializer.is_valid())
-        self.assertDictEqual({}, serializer.errors)
-
-        # common base path but different path
-        data = {"name": "fufu", "base_path": "fu/baz"}
-        serializer = DistributionSerializer(data=data)
-        self.assertTrue(serializer.is_valid())
-        self.assertDictEqual({}, serializer.errors)
-
-    def test_slashes(self):
-        overlap_errors = {"base_path": ["Relative path cannot begin or end with slashes."]}
-
-        data = {"name": "fefe", "base_path": "fefe/"}
-        serializer = DistributionSerializer(data=data)
-        self.assertFalse(serializer.is_valid())
-        self.assertDictEqual(overlap_errors, serializer.errors)
-
-        data = {"name": "fefe", "base_path": "/fefe/foo"}
-        serializer = DistributionSerializer(data=data)
-        self.assertFalse(serializer.is_valid())
-        self.assertDictEqual(overlap_errors, serializer.errors)
-
-    def test_uniqueness(self):
-        Distribution.objects.create(base_path="fizz/buzz", name="fizzbuzz")
-        data = {"name": "feefee", "base_path": "fizz/buzz"}
-        overlap_errors = {"base_path": ["This field must be unique."]}
-
-        serializer = DistributionSerializer(data=data)
-        self.assertFalse(serializer.is_valid())
-        self.assertDictEqual(overlap_errors, serializer.errors)
