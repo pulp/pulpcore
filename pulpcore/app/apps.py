c1e8fffe23831b894d63601e91f0b1b8f10ad601
pulpbot <pulp-infra@redhat.com>
2022-04-12 21:31:00 +0000

    Release 3.17.7 GH Issues: 2269,2430,2215,2061,2506
    [noissue]

--- pulpcore/app/apps.py
+++ pulpcore/app/apps.py
@@ -188,7 +188,7 @@ class PulpAppConfig(PulpPluginAppConfig):
     label = "core"
 
     # The version of this app
-    version = "3.17.7.dev"
+    version = "3.17.7"
 
     def ready(self):
         super().ready()
