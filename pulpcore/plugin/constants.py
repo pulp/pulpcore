dd5c9beb1bf709fd889ac46f4cae8cf0b2e8cfa9
Brian Bouterse <bmbouter@redhat.com>
2022-04-20 14:47:43 -0400

    Removes pulpcore.plugin.constants.API_ROOT
    Plugin writers should use the `V3_API_ROOT` or
    `V3_API_ROOT_NO_FRONT_SLASH` settings instead.
    
    closes #2556

--- pulpcore/plugin/constants.py
+++ pulpcore/plugin/constants.py
@@ -6,16 +6,3 @@ from pulpcore.constants import (  # noqa
     TASK_CHOICES,
     TASK_FINAL_STATES,
 )
-
-
-@property
-def API_ROOT():
-    from django.conf import settings
-    from pulpcore.app.loggers import deprecation_logger
-
-    deprecation_logger.warn(
-        "The API_ROOT constant has been deprecated and turned into a setting. Please use "
-        "`settings.V3_API_ROOT_NO_FRONT_SLASH` instead. This symbol will be deleted with pulpcore "
-        "3.20."
-    )
-    return settings.V3_API_ROOT_NO_FRONT_SLASH
