c4410e311bd8727e0945d64e7c0d099ed4d62c48
Matthias Dellweg <mdellweg@redhat.com>
2022-04-29 18:33:29 +0200

    Activate streaming from sftp storage tests
    [noissue]
    
    re #2537

---
+++ CHANGES/plugin_api/2537.feature
@@ -0,0 +1,2 @@
+Added ``pulpcore.plugin.content.ArtifactResponse`` to plugin API. Use this response to stream an
+artifact from the object storage if redirecting is impossible.
--- pulpcore/plugin/content.py
+++ pulpcore/plugin/content.py
@@ -1,2 +1,3 @@
 from pulpcore.content import app  # noqa
 from pulpcore.content.handler import Handler, PathNotResolved  # noqa
+from pulpcore.responses import ArtifactResponse  # noqa
