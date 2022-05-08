46fa5f291ecc9a9c40c35f00a6f57ca7079cf34f
pulpbot <pulp-infra@redhat.com>
2022-05-08 03:27:03 +0000

    Update CI files
    [noissue]

--- .ci/scripts/upper_bound.py
+++ .ci/scripts/upper_bound.py
@@ -3,9 +3,11 @@ from pkg_resources import Requirement
 packages = []
 with open("requirements.txt", "r") as fd:
     for line in fd.readlines():
-        req = Requirement.parse(line)
-        if len(req.specs) < 2 and "~=" not in str(req.specs) and "==" not in str(req.specs):
-            packages.append(req.name)
+        if not line.startswith("#"):
+            req = Requirement.parse(line)
+            spec = str(req.specs)
+            if len(req.specs) < 2 and "~=" not in spec and "==" not in spec and "<" not in spec:
+                packages.append(req.name)
 if packages:
     raise RuntimeError(
         "The following packages are missing upper bound: {}".format(", ".join(packages))
