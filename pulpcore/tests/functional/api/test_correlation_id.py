db3c078df5c2de23dc8683e285fcbff282255c15
Matthias Dellweg <mdellweg@redhat.com>
2022-05-12 17:30:43 +0200

    Rewrite correlation-id test
    [noissue]

--- pulpcore/tests/functional/api/test_correlation_id.py
+++ pulpcore/tests/functional/api/test_correlation_id.py
@@ -1,35 +1,10 @@
-import unittest
-import uuid
-
-from pulp_smash import api, cli, config
 from pulp_smash.pulp3.bindings import monitor_task
 
-from pulpcore.client.pulpcore import ApiClient, OrphansCleanupApi, TasksApi
 
-
-class CorrelationIdTestCase(unittest.TestCase):
-    """Test correlation id functionality."""
-
-    @classmethod
-    def setUpClass(cls):
-        """Set up class variables."""
-        cls.cid = str(uuid.uuid4())
-        cls.cfg = config.get_config()
-        cls.client = api.Client(cls.cfg, api.json_handler)
-        cls.client = ApiClient(
-            configuration=cls.cfg.get_bindings_config(),
-            header_name="Correlation-ID",
-            header_value=cls.cid,
-        )
-        cls.cli_client = cli.Client(cls.cfg)
-
-        cls.orphan_cleanup_api = OrphansCleanupApi(cls.client)
-        cls.task_api = TasksApi(cls.client)
-
-    def test_correlation_id(self):
-        """Test that a correlation can be passed as a header and logged."""
-        response, status, headers = self.orphan_cleanup_api.cleanup_with_http_info({})
-        monitor_task(response.task)
-        task = self.task_api.read(response.task)
-        self.assertEqual(headers["Correlation-ID"], self.cid)
-        self.assertEqual(task.logging_cid, self.cid)
+def test_correlation_id(cid, tasks_api_client, orphans_cleanup_api_client):
+    """Test that a correlation can be passed as a header and logged."""
+    response, status, headers = orphans_cleanup_api_client.cleanup_with_http_info({})
+    monitor_task(response.task)
+    task = tasks_api_client.read(response.task)
+    assert headers["Correlation-ID"] == cid
+    assert task.logging_cid == cid
