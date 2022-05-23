57c60d8ab9b34c001801f4a0c2e74b23cbee449c
pulpbot <pulp-infra@redhat.com>
2022-05-23 18:11:23 +0000

    Update CI files
    [noissue]

--- .github/workflows/scripts/install.sh
+++ .github/workflows/scripts/install.sh
@@ -22,7 +22,7 @@ if [[ "$TEST" = "docs" || "$TEST" = "publish" ]]; then
   pip install -r doc_requirements.txt
 fi
 
-pip install -e ../pulpcore
+pip install -e ../pulpcore -e ../pulp_file -e ../pulp-certguard
 pip install -r functest_requirements.txt
 
 cd .ci/ansible/
