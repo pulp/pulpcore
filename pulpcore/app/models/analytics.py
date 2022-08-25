14cedb972bb682e22f6dcb62b41e09480c8b7a22
Daniel Alley <dalley@redhat.com>
2022-08-25 00:35:08 -0400

    Switch to a UUIDv7 (draft 04) implementation as the UUID generator
    UUIDv7 was designed to be lexicographically sortable and
    database-index friendly.
    
    closes #3117

--- pulpcore/app/models/analytics.py
+++ pulpcore/app/models/analytics.py
@@ -1,12 +1,12 @@
-import uuid
-
 from django.db import models
 
 from django_lifecycle import hook, LifecycleModel
 
+from pulpcore.app.models import pulp_uuid
+
 
 class SystemID(LifecycleModel):
-    pulp_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    pulp_id = models.UUIDField(primary_key=True, default=pulp_uuid, editable=False)
 
     @hook("before_save")
     def ensure_singleton(self):
