0a507c7506810418bb7aa02e3daf09ac87acc6c0
pulpbot <pulp-infra@redhat.com>
2022-09-20 14:20:17 +0000

    Update CI files
    [noissue]

--- .github/workflows/scripts/install.sh
+++ .github/workflows/scripts/install.sh
@@ -94,7 +94,9 @@ if [ "$TEST" = "s3" ]; then
     command: "server /data"' vars/main.yaml
   sed -i -e '$a s3_test: true\
 minio_access_key: "'$MINIO_ACCESS_KEY'"\
-minio_secret_key: "'$MINIO_SECRET_KEY'"' vars/main.yaml
+minio_secret_key: "'$MINIO_SECRET_KEY'"\
+pulp_scenario_settings: null\
+' vars/main.yaml
   export PULP_API_ROOT="/rerouted/djnd/"
 fi
 
@@ -111,7 +113,9 @@ if [ "$TEST" = "azure" ]; then
     volumes:\
       - ./azurite:/etc/pulp\
     command: "azurite-blob --blobHost 0.0.0.0 --cert /etc/pulp/azcert.pem --key /etc/pulp/azkey.pem"' vars/main.yaml
-  sed -i -e '$a azure_test: true' vars/main.yaml
+  sed -i -e '$a azure_test: true\
+pulp_scenario_settings: null\
+' vars/main.yaml
 fi
 
 echo "PULP_API_ROOT=${PULP_API_ROOT}" >> "$GITHUB_ENV"
