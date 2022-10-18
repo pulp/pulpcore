d443df017aae8fc388fbc1fca638b30d9f9ff274
Andre "decko" de Brito <decko@redhat.com>
2022-10-18 15:46:08 -0300

    [noissue] Moves some tasks tests from pulp_file into pulpcore.

--- pulpcore/tests/functional/utils.py
+++ pulpcore/tests/functional/utils.py
@@ -1,5 +1,7 @@
 """Utilities for Pulpcore tests."""
 import aiohttp
+import asyncio
+from dataclasses import dataclass
 
 from functools import partial
 from time import sleep
@@ -92,3 +94,32 @@ def monitor_task_group(tg_href):
         raise PulpTaskGroupError(task_group=tg)
 
     return tg
+
+
+@dataclass
+class Download:
+    """Class for representing a downloaded file."""
+
+    body: bytes
+    response_obj: aiohttp.ClientResponse
+
+    def __init__(self, body, response_obj):
+        self.body = body
+        self.response_obj = response_obj
+
+
+def download_file(url, auth=None, headers=None):
+    """Download a file.
+
+    :param url: str URL to the file to download
+    :param auth: `aiohttp.BasicAuth` containing basic auth credentials
+    :param headers: dict of headers to send with the GET request
+    :return: Download
+    """
+    return asyncio.run(_download_file(url, auth=auth, headers=headers))
+
+
+async def _download_file(url, auth=None, headers=None):
+    async with aiohttp.ClientSession(auth=auth, raise_for_status=True) as session:
+        async with session.get(url, verify_ssl=False, headers=headers) as response:
+            return Download(body=await response.read(), response_obj=response)
