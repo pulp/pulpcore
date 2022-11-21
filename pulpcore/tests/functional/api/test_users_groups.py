3771c9b3e00efcd93c7638570aeaa2495e4b272c
Matthias Dellweg <mdellweg@redhat.com>
2022-11-21 12:03:34 +0100

    Rewrite test_users_groups to pytest style
    fixes #3334

--- pulpcore/tests/functional/api/test_users_groups.py
+++ pulpcore/tests/functional/api/test_users_groups.py
@@ -1,99 +1,60 @@
-import unittest
+import pytest
+import uuid
 
 
-from pulp_smash import api, cli, config
-
-from pulpcore.client.pulpcore import ApiClient, GroupsApi, UsersApi
-
-
-class UserTestCase(unittest.TestCase):
-    """Test REST API for users."""
-
-    @classmethod
-    def setUpClass(cls):
-        cls.cfg = config.get_config()
-        cls.client = api.Client(cls.cfg, api.json_handler)
-        cls.client = ApiClient(
-            configuration=cls.cfg.get_bindings_config(),
-        )
-        cls.cli_client = cli.Client(cls.cfg)
-
-        cls.user_api = UsersApi(cls.client)
-
-    def test_list_users(self):
-        """Test that users can be listed."""
-        users = self.user_api.list()
-        self.assertIn("admin", [user.username for user in users.results])
-
-    def test_filter_users(self):
-        """Test that users can be filterd."""
-        users = self.user_api.list(username="admin")
-        self.assertEqual(len(users.results), 1)
-
-        users = self.user_api.list(username__contains="test_")
-        self.assertEqual(len(users.results), 0)
-
-        user_hrefs = [
-            self.user_api.create(user={"username": name}).pulp_href
-            for name in ["test_newbee", "test_admin"]
-        ]
-        try:
-            users = self.user_api.list(username__contains="test_")
-            self.assertEqual(len(users.results), 2)
-            users = self.user_api.list(username__contains="test_new")
-            self.assertEqual(len(users.results), 1)
-            users = self.user_api.list(username="test_newbee")
-            self.assertEqual(len(users.results), 1)
-        finally:
-            for user_href in user_hrefs:
-                self.user_api.delete(user_href)
+@pytest.mark.parallel
+def test_list_users(users_api_client):
+    users = users_api_client.list()
+    assert "admin" in [user.username for user in users.results]
 
 
-class GroupTestCase(unittest.TestCase):
-    """Test REST API for users."""
+@pytest.mark.parallel
+def test_filter_users(users_api_client, gen_user):
+    """Test that users can be filterd."""
+    prefix = str(uuid.uuid4())
+    users = users_api_client.list(username__contains=f"{prefix}_")
+    assert len(users.results) == 0
 
-    @classmethod
-    def setUpClass(cls):
-        cls.cfg = config.get_config()
-        cls.client = api.Client(cls.cfg, api.json_handler)
-        cls.client = ApiClient(
-            configuration=cls.cfg.get_bindings_config(),
-        )
-        cls.cli_client = cli.Client(cls.cfg)
+    gen_user(username=f"{prefix}_newbee")
+    gen_user(username=f"{prefix}_admin")
 
-        cls.group_api = GroupsApi(cls.client)
+    users = users_api_client.list(username__contains=f"{prefix}_")
+    assert len(users.results) == 2
+    users = users_api_client.list(username__contains=f"{prefix}_new")
+    assert len(users.results) == 1
+    users = users_api_client.list(username=f"{prefix}_newbee")
+    assert len(users.results) == 1
 
-    def test_list_groups(self):
-        """Test that a group can be crud."""
 
-        groups = self.group_api.list()
-        self.assertNotIn("test_newbees", [group.name for group in groups.results])
-        group_href = self.group_api.create(group={"name": "test_newbees"}).pulp_href
-        try:
-            groups = self.group_api.list()
-            self.assertIn("test_newbees", [group.name for group in groups.results])
-        finally:
-            self.group_api.delete(group_href)
-        groups = self.group_api.list()
-        self.assertNotIn("test_newbees", [group.name for group in groups.results])
+@pytest.mark.parallel
+def test_crd_groups(groups_api_client, gen_object_with_cleanup):
+    """Test that a group can be crd."""
 
-    def test_filter_groups(self):
-        """Test that groups can be filterd."""
+    prefix = str(uuid.uuid4())
+    groups = groups_api_client.list()
+    assert f"{prefix}_newbees" not in [group.name for group in groups.results]
+    group_href = gen_object_with_cleanup(groups_api_client, {"name": f"{prefix}_newbees"}).pulp_href
+    groups = groups_api_client.list()
+    assert f"{prefix}_newbees" in [group.name for group in groups.results]
+    groups_api_client.delete(group_href)
+    groups = groups_api_client.list()
+    assert f"{prefix}_newbees" not in [group.name for group in groups.results]
 
-        groups = self.group_api.list(name__contains="test_")
-        self.assertEqual(len(groups.results), 0)
 
-        group_hrefs = [
-            self.group_api.create(group={"name": name}).pulp_href
-            for name in ["test_newbees", "test_admins"]
-        ]
-        try:
-            groups = self.group_api.list(name__contains="test_")
-            self.assertEqual(len(groups.results), 2)
-            groups = self.group_api.list(name__contains="test_new")
-            self.assertEqual(len(groups.results), 1)
-            groups = self.group_api.list(name="test_newbees")
-            self.assertEqual(len(groups.results), 1)
-        finally:
-            for group_href in group_hrefs:
-                self.group_api.delete(group_href)
+@pytest.mark.parallel
+def test_filter_groups(groups_api_client, gen_object_with_cleanup):
+    """Test that groups can be filterd."""
+
+    prefix = str(uuid.uuid4())
+    groups = groups_api_client.list(name__contains=f"{prefix}_")
+    assert len(groups.results) == 0
+
+    gen_object_with_cleanup(groups_api_client, {"name": f"{prefix}_newbees"}).pulp_href
+    gen_object_with_cleanup(groups_api_client, {"name": f"{prefix}_admins"}).pulp_href
+
+    groups = groups_api_client.list(name__contains=f"{prefix}_")
+    assert len(groups.results) == 2
+    groups = groups_api_client.list(name__contains=f"{prefix}_new")
+    assert len(groups.results) == 1
+    groups = groups_api_client.list(name=f"{prefix}_newbees")
+    assert len(groups.results) == 1
