78a2e52b35a1fe88c1748b976ee3e1ee13bc31b5
Matthias Dellweg <mdellweg@redhat.com>
2023-01-11 10:26:32 +0100

    Fix the label migration code
    A not null constraint exception was risen when objects with and without
    labels of the same type existed. The fix will limit the update to only
    those objects that actually had labels.
    
    fixes #3495
    
    (cherry picked from commit 762c14caf1f2ae0763bfa91db25c25a48635b2ab)

--- pulpcore/app/apps.py
+++ pulpcore/app/apps.py
@@ -408,7 +408,9 @@ def _migrate_remaining_labels(sender, apps, verbosity, **kwargs):
                 .annotate(label_data=RawSQL("hstore(array_agg(key), array_agg(value))", []))
                 .values("label_data")
             )
-            model.objects.update(pulp_labels=label_subq)
+            model.objects.annotate(old_labels=label_subq).exclude(old_labels={}).update(
+                pulp_labels=label_subq
+            )
             Label.objects.filter(content_type=ctype).delete()
 
     if Label.objects.count():
