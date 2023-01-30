dea600fe6674e68dd7c6fdd50630f000af8773bb
Matthias Dellweg <mdellweg@redhat.com>
2023-01-30 15:04:26 +0100

    Repeat to kill the process of a canceled task
    fixes #3407

--- pulpcore/app/tasks/test.py
+++ pulpcore/app/tasks/test.py
@@ -1,3 +1,14 @@
+import backoff
+
+
 def dummy_task():
     """Dummy task, that can be used in tests."""
     pass
+
+
+@backoff.on_exception(backoff.expo, BaseException)
+def gooey_task(interval):
+    """A sleep task that tries to avoid being killed by ignoring all exceptions."""
+    from time import sleep
+
+    sleep(interval)
