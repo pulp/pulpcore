451d5462cab69792e57c005c771b6f2748d53fdf
Brian Bouterse <bmbouter@redhat.com>
2023-03-03 13:01:09 -0500

    Adds otel WSGI auto-instrumentation [noissue]

--- pulpcore/app/wsgi.py
+++ pulpcore/app/wsgi.py
@@ -9,9 +9,12 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application  # noqa
+from django.core.wsgi import get_wsgi_application
+from opentelemetry.instrumentation.wsgi import OpenTelemetryMiddleware
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pulpcore.app.settings")
 
 
 application = get_wsgi_application()
+application = OpenTelemetryMiddleware(application)
