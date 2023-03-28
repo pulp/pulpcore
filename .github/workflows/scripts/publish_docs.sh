87eb4855078b3b87f5a95f41858465ce94a56318
pulpbot <pulp-infra@redhat.com>
2023-03-28 08:05:41 +0000

    Update CI files
    [noissue]

--- .github/workflows/scripts/publish_docs.sh
+++ .github/workflows/scripts/publish_docs.sh
@@ -17,8 +17,8 @@ touch ~/.ssh/pulp-infra
 chmod 600 ~/.ssh/pulp-infra
 echo "$PULP_DOCS_KEY" > ~/.ssh/pulp-infra
 
-echo "docs.pulpproject.org,8.43.85.236 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBGXG+8vjSQvnAkq33i0XWgpSrbco3rRqNZr0SfVeiqFI7RN/VznwXMioDDhc+hQtgVhd6TYBOrV07IMcKj+FAzg=" >> /home/runner/.ssh/known_hosts
-chmod 644 /home/runner/.ssh/known_hosts
+echo "docs.pulpproject.org,8.43.85.236 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBGXG+8vjSQvnAkq33i0XWgpSrbco3rRqNZr0SfVeiqFI7RN/VznwXMioDDhc+hQtgVhd6TYBOrV07IMcKj+FAzg=" >> ~/.ssh/known_hosts
+chmod 644 ~/.ssh/known_hosts
 
 pip3 install packaging
 
