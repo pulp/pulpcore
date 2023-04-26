ca74650cddbd4194dbc988d48a684159accddae0
Matthias Dellweg <mdellweg@redhat.com>
2023-04-26 11:03:29 +0200

    Refactored async unittests to use pytest-async
    This fixes some warnings from the seemingly unmaintained asynctest
    library, whose dependency thereby got removed.
    
    [noissue]

--- pulpcore/tests/unit/download/test_factory.py
+++ pulpcore/tests/unit/download/test_factory.py
@@ -1,35 +1,32 @@
-from django.test import TestCase
-from asgiref.sync import sync_to_async
+import pytest
 
 from pulpcore.download.factory import DownloaderFactory
 from pulpcore.plugin.models import Remote
 
 
-class DownloaderFactoryHeadersTestCase(TestCase):
-    async def test_user_agent_header(self):
-        remote = await sync_to_async(Remote.objects.create)(url="http://example.org/", name="foo")
-        factory = DownloaderFactory(remote)
-        downloader = factory.build(remote.url)
-        default_user_agent = DownloaderFactory.user_agent()
-        self.assertEqual(downloader.session.headers["User-Agent"], default_user_agent)
-        await sync_to_async(remote.delete)()
+@pytest.mark.asyncio
+async def test_user_agent_header():
+    remote = Remote(url="http://example.org/", name="foo")
+    factory = DownloaderFactory(remote)
+    downloader = factory.build(remote.url)
+    default_user_agent = DownloaderFactory.user_agent()
+    assert downloader.session.headers["User-Agent"] == default_user_agent
 
-    async def test_custom_user_agent_header(self):
-        remote = await sync_to_async(Remote.objects.create)(
-            url="http://example.org/", headers=[{"User-Agent": "foo"}], name="foo"
-        )
-        factory = DownloaderFactory(remote)
-        downloader = factory.build(remote.url)
-        default_user_agent = DownloaderFactory.user_agent()
-        expected_user_agent = f"{default_user_agent}, foo"
-        self.assertEqual(downloader.session.headers["User-Agent"], expected_user_agent)
-        await sync_to_async(remote.delete)()
 
-    async def test_custom_headers(self):
-        remote = await sync_to_async(Remote.objects.create)(
-            url="http://example.org/", headers=[{"Connection": "keep-alive"}], name="foo"
-        )
-        factory = DownloaderFactory(remote)
-        downloader = factory.build(remote.url)
-        self.assertEqual(downloader.session.headers["Connection"], "keep-alive")
-        await sync_to_async(remote.delete)()
+@pytest.mark.asyncio
+async def test_custom_user_agent_header():
+    remote = Remote(url="http://example.org/", headers=[{"User-Agent": "foo"}], name="foo")
+    factory = DownloaderFactory(remote)
+    downloader = factory.build(remote.url)
+    default_user_agent = DownloaderFactory.user_agent()
+    expected_user_agent = f"{default_user_agent}, foo"
+    assert downloader.session.headers["User-Agent"] == expected_user_agent
+
+
+@pytest.mark.asyncio
+@pytest.mark.django_db
+async def test_custom_headers():
+    remote = Remote(url="http://example.org/", headers=[{"Connection": "keep-alive"}], name="foo")
+    factory = DownloaderFactory(remote)
+    downloader = factory.build(remote.url)
+    assert downloader.session.headers["Connection"] == "keep-alive"
