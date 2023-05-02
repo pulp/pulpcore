f37b3c98884e9dea9ec29bc75ed340c2c2d8bc35
pulpbot <pulp-infra@redhat.com>
2023-05-02 16:06:08 +0000

    Bump minor version [noissue]

--- pulpcore/app/apps.py
+++ pulpcore/app/apps.py
@@ -235,7 +235,7 @@ class PulpAppConfig(PulpPluginAppConfig):
     label = "core"
 
     # The version of this app
-    version = "3.24.0.dev"
+    version = "3.25.0.dev"
 
     # The python package name providing this app
     python_package_name = "pulpcore"
