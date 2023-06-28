091e0c4a5a3b75c506e9a62f1becc2be7f8e3871
Partha Aji <paji@redhat.com>
2023-06-28 17:41:55 -0400

    Incremental export for file exporter will now include published metadata
    fixes #3941
    
    Update CHANGES/3941.bugfix
    
    Co-authored-by: Matthias Dellweg <2500@gmx.de>
    (cherry picked from commit 405a3f7280ef5b7f688b0092c8cbeed0c914993a)

--- pulpcore/app/tasks/export.py
+++ pulpcore/app/tasks/export.py
@@ -143,12 +143,17 @@ def _export_publication_to_file_system(
             for ca in content_artifacts.select_related("artifact").iterator()
         }
 
+    publication_metadata_paths = set(
+        publication.published_metadata.values_list("relative_path", flat=True)
+    )
     for pa in publication.published_artifact.select_related(
         "content_artifact", "content_artifact__artifact"
     ).iterator():
         # Artifact isn't guaranteed to be present
         if pa.content_artifact.artifact and (
-            start_repo_version is None or pa.content_artifact.pk in difference_content_artifacts
+            start_repo_version is None
+            or pa.relative_path in publication_metadata_paths
+            or pa.content_artifact.pk in difference_content_artifacts
         ):
             relative_path_to_artifacts[pa.relative_path] = pa.content_artifact.artifact
 
