7f7b4251616d8e6cfbed4e12a69992393bef142a
pulpbot <pulp-infra@redhat.com>
2023-07-19 17:24:22 +0000

    Release 3.23.9 GH Issues: 3977
    [noissue]

--- pulpcore/app/apps.py
+++ pulpcore/app/apps.py
@@ -233,7 +233,7 @@ class PulpAppConfig(PulpPluginAppConfig):
     label = "core"
 
     # The version of this app
-    version = "3.23.9.dev"
+    version = "3.23.9"
 
     # The python package name providing this app
     python_package_name = "pulpcore"
