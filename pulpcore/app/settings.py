ff0c9be174b21057ec33c681620fb957ab91beab
Grant Gainey <ggainey@redhat.com>
2023-07-26 15:37:42 -0400

    Taught import to be able to use less than all-available workers.
    IMPORT_WORKERS_PERCENT is configurable in settings. We will
    document/expose this in a future PR, to keep this one maximally
    backportable. Default behavior remains "all workers".
    
    fixes #4068.
    
    (cherry picked from commit b5cb1d19a130b67aff5c818e1a8b1ce1e8655a67)

--- pulpcore/app/settings.py
+++ pulpcore/app/settings.py
@@ -296,6 +296,10 @@ ALLOWED_CONTENT_CHECKSUMS = ["sha224", "sha256", "sha384", "sha512"]
 
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
+# What percentage of available-workers will pulpimport use at a time, max
+# By default, use all available workers.
+IMPORT_WORKERS_PERCENT = 100
+
 # HERE STARTS DYNACONF EXTENSION LOAD (Keep at the very bottom of settings.py)
 # Read more at https://dynaconf.readthedocs.io/en/latest/guides/django.html
 import dynaconf  # noqa
