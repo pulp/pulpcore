8681b7ca1c788ca04681488eb21ed49808fd58ef
Matthias Dellweg <mdellweg@redhat.com>
2023-09-04 16:21:13 +0200

    Fix encrypted fields bulk_update corrupting data
    fixes #4359
    
    (cherry picked from commit db05a31e5643bdff66fe3573b997e08fe70bb4f6)

--- pulpcore/tests/unit/models/test_remote.py
+++ pulpcore/tests/unit/models/test_remote.py
@@ -1,19 +1,37 @@
 import pytest
 from uuid import uuid4
+from cryptography.fernet import InvalidToken
 
+from django.core.management import call_command
 from django.db import connection
 
-from pulpcore.app.models import Remote
+from pulpcore.app.models import Remote, Domain
 from pulpcore.app.models.fields import _fernet, EncryptedTextField
+
+
+TEST_KEY1 = b"hPCIFQV/upbvPRsEpgS7W32XdFA2EQgXnMtyNAekebQ="
+TEST_KEY2 = b"6Xyv+QezAQ+4R870F5qsgKcngzmm46caDB2gyo9qnpc="
 
 
 @pytest.fixture
 def fake_fernet(tmp_path, settings):
+    def _steps():
+        yield
+        key_file.write_bytes(TEST_KEY2 + b"\n" + TEST_KEY1)
+        _fernet.cache_clear()
+        yield
+        key_file.write_bytes(TEST_KEY2)
+        _fernet.cache_clear()
+        yield
+        key_file.write_bytes(TEST_KEY1)
+        _fernet.cache_clear()
+        yield
+
     key_file = tmp_path / "db_symmetric_key"
-    key_file.write_bytes(b"hPCIFQV/upbvPRsEpgS7W32XdFA2EQgXnMtyNAekebQ=")
+    key_file.write_bytes(TEST_KEY1)
     settings.DB_ENCRYPTION_KEY = str(key_file)
     _fernet.cache_clear()
-    yield
+    yield _steps()
     _fernet.cache_clear()
 
 
@@ -31,3 +49,29 @@ def test_encrypted_proxy_password(fake_fernet):
     proxy_password = EncryptedTextField().from_db_value(db_proxy_password, None, connection)
     assert db_proxy_password != "test"
     assert proxy_password == "test"
+
+
+@pytest.mark.django_db
+def test_rotate_db_key(fake_fernet):
+    remote = Remote.objects.create(name=uuid4(), proxy_password="test")
+    domain = Domain.objects.create(name=uuid4(), storage_settings={"base_path": "/foo"})
+
+    next(fake_fernet)  # new + old key
+
+    call_command("rotate-db-key")
+
+    next(fake_fernet)  # new key
+
+    del remote.proxy_password
+    assert remote.proxy_password == "test"
+    del domain.storage_settings
+    assert domain.storage_settings == {"base_path": "/foo"}
+
+    next(fake_fernet)  # old key
+
+    del remote.proxy_password
+    with pytest.raises(InvalidToken):
+        remote.proxy_password
+    del domain.storage_settings
+    with pytest.raises(InvalidToken):
+        domain.storage_settings
