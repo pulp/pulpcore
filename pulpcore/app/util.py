6f557e5c046c8c917a192260b48ac99811f01405
Daniel Alley <dalley@redhat.com>
2023-09-19 01:29:07 -0400

    Clean up file checksum utilities
    [noissue]
    
    (cherry picked from commit a3a4b82f5df813ef5b57cbb8109192440edfee00)

--- pulpcore/app/util.py
+++ pulpcore/app/util.py
@@ -1,3 +1,4 @@
+import hashlib
 import os
 
 from django.apps import apps
@@ -159,3 +160,16 @@ def get_request_without_query_params(context):
         request.query_params._mutable = False
 
     return request
+
+
+def compute_file_hash(filename, hasher=None, cumulative_hash=None, blocksize=8192):
+    if hasher is None:
+        hasher = hashlib.sha256()
+
+    with open(filename, "rb") as f:
+        # Read and update hash string value in blocks of 8K
+        while chunk := f.read(blocksize):
+            hasher.update(chunk)
+            if cumulative_hash:
+                cumulative_hash.update(chunk)
+        return hasher.hexdigest()
