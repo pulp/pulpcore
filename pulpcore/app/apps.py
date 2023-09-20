c879f6a390730114721c9bb1074befa8db12e7f6
pulpbot <pulp-infra@redhat.com>
2023-09-20 05:32:34 +0000

    Bump to 3.16.21.dev
    [noissue]

--- pulpcore/app/apps.py
+++ pulpcore/app/apps.py
@@ -185,7 +185,7 @@ class PulpAppConfig(PulpPluginAppConfig):
     label = "core"
 
     # The version of this app
-    version = "3.16.20"
+    version = "3.16.21.dev"
 
     def ready(self):
         super().ready()
