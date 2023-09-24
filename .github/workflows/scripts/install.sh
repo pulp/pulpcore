cd5955a218df3b961a763df7719bcf4f3e7b6e53
pulpbot <pulp-infra@redhat.com>
2023-09-24 17:35:50 +0000

    Update CI files
    [noissue]

--- .github/workflows/scripts/install.sh
+++ .github/workflows/scripts/install.sh
@@ -68,6 +68,7 @@ services:
 VARSYAML
 
 cat >> vars/main.yaml << VARSYAML
+pulp_env: {}
 pulp_settings: {"allowed_export_paths": ["/tmp"], "allowed_import_paths": ["/tmp"], "orphan_protection_time": 0}
 pulp_scheme: https
 
@@ -99,6 +100,7 @@ if [ "$TEST" = "s3" ]; then
 minio_access_key: "'$MINIO_ACCESS_KEY'"\
 minio_secret_key: "'$MINIO_SECRET_KEY'"\
 pulp_scenario_settings: {"domain_enabled": true, "hide_guarded_distributions": true}\
+pulp_scenario_env: {}\
 ' vars/main.yaml
   export PULP_API_ROOT="/rerouted/djnd/"
 fi
@@ -118,6 +120,7 @@ if [ "$TEST" = "azure" ]; then
     command: "azurite-blob --blobHost 0.0.0.0 --cert /etc/pulp/azcert.pem --key /etc/pulp/azkey.pem"' vars/main.yaml
   sed -i -e '$a azure_test: true\
 pulp_scenario_settings: {"domain_enabled": true}\
+pulp_scenario_env: {}\
 ' vars/main.yaml
 fi
 
