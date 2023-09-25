9bcd7fb922c4d0a953c00816669452442dbe3484
Daniel Alley <dalley@redhat.com>
2023-09-25 14:40:12 -0400

    Add a fast, opt-in, backwards-incompatible export mode
    The goal being to make changes to the way import / export is handled
    that improve performance, but can't be shipped in a bugfix without
    potentially breaking existing exports.
    
    (cherry picked from commit 5b3f3dfa0ea7123f25bfa66356e61ab622d0ab37)

--- pulpcore/app/util.py
+++ pulpcore/app/util.py
@@ -259,14 +259,15 @@ def configure_telemetry():
         models.TaskSchedule.objects.filter(task_name=task_name).delete()
 
 
-def compute_file_hash(filename, hasher=None, cumulative_hash=None, blocksize=8192):
+def compute_file_hash(filename, hasher=None, cumulative_hash=None, blocksize=16384):
     if hasher is None:
         hasher = hashlib.sha256()
 
     with open(filename, "rb") as f:
-        # Read and update hash string value in blocks of 8K
+        # Read and update hash string value in blocks of 16K
         while chunk := f.read(blocksize):
             hasher.update(chunk)
             if cumulative_hash:
                 cumulative_hash.update(chunk)
         return hasher.hexdigest()
+
