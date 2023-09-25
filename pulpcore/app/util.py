99dba8353b97685a1031419353f1a6c980c43328
Daniel Alley <dalley@redhat.com>
2023-09-25 14:40:12 -0400

    Add a fast, opt-in, backwards-incompatible export mode
    The goal being to make changes to the way import / export is handled
    that improve performance, but can't be shipped in a bugfix without
    potentially breaking existing exports.
    
    (cherry picked from commit 5b3f3dfa0ea7123f25bfa66356e61ab622d0ab37)

--- pulpcore/app/util.py
+++ pulpcore/app/util.py
@@ -248,6 +248,19 @@ def gpg_verify(public_keys, signature, detached_data=None):
     return verified
 
 
+def compute_file_hash(filename, hasher=None, cumulative_hash=None, blocksize=16384):
+    if hasher is None:
+        hasher = hashlib.sha256()
+
+    with open(filename, "rb") as f:
+        # Read and update hash string value in blocks of 16K
+        while chunk := f.read(blocksize):
+            hasher.update(chunk)
+            if cumulative_hash:
+                cumulative_hash.update(chunk)
+        return hasher.hexdigest()
+
+
 def configure_analytics():
     task_name = "pulpcore.app.tasks.analytics.post_analytics"
     dispatch_interval = timedelta(days=1)
@@ -305,16 +318,3 @@ def get_artifact_url(artifact, headers=None, http_method=None):
             "does not allow redirecting."
         )
     return url
-
-
-def compute_file_hash(filename, hasher=None, cumulative_hash=None, blocksize=8192):
-    if hasher is None:
-        hasher = hashlib.sha256()
-
-    with open(filename, "rb") as f:
-        # Read and update hash string value in blocks of 8K
-        while chunk := f.read(blocksize):
-            hasher.update(chunk)
-            if cumulative_hash:
-                cumulative_hash.update(chunk)
-        return hasher.hexdigest()
