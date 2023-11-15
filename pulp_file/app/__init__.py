a5d110ebad309cf89f52d129a203cc9ac85d8909
Matthias Dellweg <mdellweg@redhat.com>
2023-11-15 16:20:40 +0100

    Fix pulp_file package name in status
    Status report of the pulp_file python_package must be pulp-file.
    Technically this is still wrong, because it should be pulpcore, but we
    will need another field to present the python module in order to be able
    to generate the bindings.
    
    fixes #4724

--- pulp_file/app/__init__.py
+++ pulp_file/app/__init__.py
@@ -9,5 +9,5 @@ class PulpFilePluginAppConfig(PulpPluginAppConfig):
     name = "pulp_file.app"
     label = "file"
     version = "3.42.0.dev"
-    python_package_name = "pulp_file"  # TODO Add python_module_name
+    python_package_name = "pulp-file"  # TODO Add python_module_name
     domain_compatible = True
