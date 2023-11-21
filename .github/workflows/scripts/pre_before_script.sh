aa1ba59e33e36b77a77f2cedfa3da9607ef1ab8a
pulpbot <pulp-infra@redhat.com>
2023-11-21 21:59:22 +0000

    Update CI files
    [noissue]

--- .github/workflows/scripts/pre_before_script.sh
+++ .github/workflows/scripts/pre_before_script.sh
@@ -1,6 +1,6 @@
 set -euv
 
 if [[ "$TEST" == "azure" ]]; then
-    cmd_stdin_prefix bash -c "cat > /var/lib/pulp/scripts/otel_server.py" < "$GITHUB_WORKSPACE"/pulpcore/tests/functional/assets/otel_server.py
+    cmd_stdin_prefix bash -c "cat > /var/lib/pulp/scripts/otel_server.py" < "pulpcore/tests/functional/assets/otel_server.py"
     docker exec -d "$PULP_CI_CONTAINER" "$@" su pulp -c "python3 /var/lib/pulp/scripts/otel_server.py"
 fi
