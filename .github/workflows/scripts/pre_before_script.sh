6da95a33c34a109110b6abd1a09d5a64a9181e62
Matthias Dellweg <mdellweg@redhat.com>
2023-11-21 07:41:04 +0100

    EXPERIMENT: Update CI
    [noissue]

--- .github/workflows/scripts/pre_before_script.sh
+++ .github/workflows/scripts/pre_before_script.sh
@@ -1,7 +1,5 @@
 set -euv
 
-
-
 if [ "$TEST" = "azure" ]; then
     cmd_stdin_prefix bash -c "cat > /var/lib/pulp/scripts/otel_server.py" < pulpcore/tests/functional/assets/otel_server.py
     cmd_user_prefix nohup python3 /var/lib/pulp/scripts/otel_server.py &
