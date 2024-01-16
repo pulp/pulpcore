2207a3bc9f04dcef51e0ef3c1d389ecda84484d9
pulpbot <pulp-infra@redhat.com>
2024-01-16 14:58:46 +0000

    Bump minor version [noissue]

--- CHANGES/4917.feature
+++
@@ -1,1 +0,0 @@
-Enabled the gunicorn applications for ``pulpcore-api`` and ``pulpcore-content`` to load configurations from the "gunicorn.conf.py" file.
--- pulp_file/app/__init__.py
+++ pulp_file/app/__init__.py
@@ -8,6 +8,6 @@ class PulpFilePluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_file.app"
     label = "file"
-    version = "3.44.0.dev"
+    version = "3.45.0.dev"
     python_package_name = "pulpcore"
     domain_compatible = True
