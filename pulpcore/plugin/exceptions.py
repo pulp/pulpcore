fa7b90cdf5ed77dc6a254e1c237d94c703f1cf72
Lubos Mjachky <lmjachky@redhat.com>
2024-02-03 22:31:55 +0100

    Expose TimeoutException
    closes #5024

--- pulpcore/plugin/exceptions.py
+++ pulpcore/plugin/exceptions.py
@@ -4,5 +4,6 @@ from pulpcore.exceptions import (  # noqa: F401
     PulpException,
     SizeValidationError,
     MissingDigestValidationError,
+    TimeoutException,
     UnsupportedDigestValidationError,
 )
