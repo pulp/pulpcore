605075f33569c4948fc74c125b6a4a2f219e94ef
Quirin Pamp <pamp@atix.de>
2024-02-27 18:13:56 +0100

    Fix NoArtifactContentUploadViewSet when no file is provided

--- pulpcore/plugin/viewsets/content.py
+++ pulpcore/plugin/viewsets/content.py
@@ -44,17 +44,18 @@ class NoArtifactContentUploadViewSet(DefaultDeferredContextMixin, ContentViewSet
 
         task_payload = {k: v for k, v in request.data.items()}
 
+        context = self.get_deferred_context(request)
         file_content = task_payload.pop("file", None)
-        temp_file = PulpTemporaryFile.init_and_validate(file_content)
-        temp_file.save()
+        if file_content:
+            temp_file = PulpTemporaryFile.init_and_validate(file_content)
+            temp_file.save()
+            context["pulp_temp_file_pk"] = str(temp_file.pk)
 
         exclusive_resources = [
             item for item in (serializer.validated_data.get(key) for key in ("repository",)) if item
         ]
 
         app_label = self.queryset.model._meta.app_label
-        context = self.get_deferred_context(request)
-        context["pulp_temp_file_pk"] = str(temp_file.pk)
         task = dispatch(
             tasks.base.general_create,
             exclusive_resources=exclusive_resources,
