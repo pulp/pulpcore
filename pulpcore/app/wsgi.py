ac01d8517425e09133eefbdf28ec4dd19ddc0eaf
Gerrod Ubben <gerrodubben@gmail.com>
2024-03-26 10:18:48 -0400

    Fix import in wsgi preventing startup
    Fixes #5189
    
    (cherry picked from commit 37a3c8661d5fccaa52b7ac83c08bdaa12c4e2db9)

--- pulpcore/app/wsgi.py
+++ pulpcore/app/wsgi.py
@@ -11,12 +11,13 @@ from django.core.wsgi import get_wsgi_application
 from opentelemetry.instrumentation.wsgi import OpenTelemetryMiddleware
 
 from pulpcore.app.entrypoint import using_pulp_api_worker
-from pulpcore.app.util import init_domain_metrics_exporter
 
 if not using_pulp_api_worker.get(False):
     raise RuntimeError("This app must be executed using pulpcore-api entrypoint.")
 
 application = get_wsgi_application()
 application = OpenTelemetryMiddleware(application)
+
+from pulpcore.app.util import init_domain_metrics_exporter  # noqa: E402
 
 init_domain_metrics_exporter()
