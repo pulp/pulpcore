12926ccb70506eae8b1b3d4176b26b882a766380
pulpbot <pulp-infra@redhat.com>
2024-03-26 19:04:15 +0000

    Bump version: 3.45.4 → 3.45.5.dev

--- pulp_certguard/app/__init__.py
+++ pulp_certguard/app/__init__.py
@@ -6,6 +6,6 @@ class PulpCertGuardPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_certguard.app"
     label = "certguard"
-    version = "3.45.4"
+    version = "3.45.5.dev"
     python_package_name = "pulpcore"
     domain_compatible = True
