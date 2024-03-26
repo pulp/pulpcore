03cfb80751db79a6cd7f8fd7ebf32398adfd890d
Dennis Kliban <dkliban@redhat.com>
2024-03-26 16:52:41 -0400

    Change URL for pulp-docs repo
    [noissue]

--- .github/workflows/scripts/build_all_docs.sh
+++ .github/workflows/scripts/build_all_docs.sh
@@ -1,4 +1,4 @@
 # This script builds the documentation site for staging-docs.pulpproject.org
-pip install git+https://github.com/pedro-psb/pulp-docs.git
+pip install git+https://github.com/pulp/pulp-docs.git
 pulp-docs build
 tar cvf staging-docs.pulpproject.org.tar ./site
