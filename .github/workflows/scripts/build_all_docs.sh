c2a558c7d17d96a32144ca6c8cc2462cb207a4e9
Dennis Kliban <dkliban@redhat.com>
2024-03-27 09:08:31 -0400

    Revert "Change dir before building docs"
    This reverts commit f8fdec326d2710e034057c59938e901d082f9227.

--- .github/workflows/scripts/build_all_docs.sh
+++ .github/workflows/scripts/build_all_docs.sh
@@ -1,6 +1,4 @@
 # This script builds the documentation site for staging-docs.pulpproject.org
 pip install git+https://github.com/pulp/pulp-docs.git
-cd ..
 pulp-docs build
-tar cvf pulpcore/staging-docs.pulpproject.org.tar ./site
-cd pulpcore
+tar cvf staging-docs.pulpproject.org.tar ./site
