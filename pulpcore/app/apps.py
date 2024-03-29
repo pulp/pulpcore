337247c0f834ca7815c9159d747b83ece9c7c756
Grant Gainey <ggainey@redhat.com>
2024-03-29 10:57:52 -0400

    Revert "Bump version: 3.18.36 → 3.18.37.dev"
    This reverts commit 748afcca0e611f37048c463cb57521470c3f3a48.

--- pulpcore/app/apps.py
+++ pulpcore/app/apps.py
@@ -188,7 +188,7 @@ class PulpAppConfig(PulpPluginAppConfig):
     label = "core"
 
     # The version of this app
-    version = "3.18.37.dev"
+    version = "3.18.36"
 
     def ready(self):
         super().ready()
