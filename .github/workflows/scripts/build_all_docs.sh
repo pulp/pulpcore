717b70a06b884aff8b577e3b734a5a95def00386
Pedro Brochado <pedropsb95@gmail.com>
2024-04-12 14:01:17 -0300

    Remove docs-build workaround
    The workaround was fixed by mkdocstrings-python 1.9.1 and pulp-docs
    pinned its dependencies to >=1.9.1
    
    pulp-docs-pin: https://github.com/pulp/pulp-docs/pull/37
    
    [noissue]

--- .github/workflows/scripts/build_all_docs.sh
+++ .github/workflows/scripts/build_all_docs.sh
@@ -1,11 +1,5 @@
 # This script builds the documentation site for staging-docs.pulpproject.org
 
-# TODO: this chdir move is a tmp workaround. Remove when no longer needed.
-# see: https://github.com/mkdocstrings/python/issues/145
-mkdir ../build_dir && pushd ../build_dir
-
 pip install git+https://github.com/pulp/pulp-docs.git
 pulp-docs build
-tar cvf ../pulpcore/staging-docs.pulpproject.org.tar site
-
-popd
+tar cvf staging-docs.pulpproject.org.tar site
