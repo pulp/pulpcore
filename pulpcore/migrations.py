a7da0bd02b26fd7c34aa5762268a82d9da9bf61f
Matthias Dellweg <mdellweg@redhat.com>
2024-04-26 14:10:41 +0200

    Fix RequireVersion missing backwards methods
    [noissue]
    
    (cherry picked from commit b25678892d29d4c944e21aa728eb16fcdff2826c)

--- pulpcore/migrations.py
+++ pulpcore/migrations.py
@@ -17,6 +17,9 @@ class RequireVersion(Operation):
     def state_forwards(self, app_label, state):
         pass
 
+    def state_backwards(self, app_label, state):
+        pass
+
     def database_forwards(self, app_label, schema_editor, from_state, to_state):
         from_state.clear_delayed_apps_cache()
         ApiAppStatus = from_state.apps.get_model("core", "ApiAppStatus")
@@ -53,6 +56,9 @@ class RequireVersion(Operation):
                     ]
                 )
             )
+
+    def database_backwards(self, app_label, schema_editor, from_state, to_state):
+        pass
 
     def describe(self):
         return f"Require plugin '{self.plugin}' >= '{self.version}'."
