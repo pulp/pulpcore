1798991cf19bf4917d2dd2ac2497c607c299cc53
pulpbot <pulp-infra@redhat.com>
2024-05-13 11:52:26 +0000

    Update CI files
    [noissue]

--- docs/conf.py
+++ docs/conf.py
@@ -310,3 +310,8 @@ class MyPythonDomain(PythonDomain):
 def setup(sphinx):
     """Use MyPythonDomain in place of PythonDomain"""
     sphinx.add_domain(MyPythonDomain, override=True)
+
+rst_prolog = """.. attention::
+    This documentation will be deactivated in the near future. `Learn More <https://discourse.pulpproject.org/t/unified-documentation-part-2/1188>`_
+    or go to the `New Pulp Docs <https://staging-docs.pulpproject.org/>`_ (beta).
+"""
