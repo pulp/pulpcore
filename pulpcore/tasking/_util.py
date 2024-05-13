3b8b14fb4714f5fad95a97372e299671d37a9cb9
Matthias Dellweg <mdellweg@redhat.com>
2024-05-13 16:48:15 +0200

    Stop task cleanup to delete Contents and Artifacts
    These shared objects that may not even have been created by that task
    must only ever be deleted inside of orphan cleanup. That's the rule.
    
    fixes #5363

--- pulpcore/tasking/_util.py
+++ pulpcore/tasking/_util.py
@@ -15,7 +15,7 @@ from django.db.models import Q
 from django.utils import timezone
 from django_guid import set_guid
 from django_guid.utils import generate_guid
-from pulpcore.app.models import Task, TaskSchedule
+from pulpcore.app.models import Artifact, Content, Task, TaskSchedule
 from pulpcore.app.role_util import get_users_with_perms
 from pulpcore.app.util import (
     set_current_user,
@@ -73,6 +73,8 @@ def delete_incomplete_resources(task):
     if task.state != TASK_STATES.CANCELING:
         raise RuntimeError(_("Task must be canceling."))
     for model in (r.content_object for r in task.created_resources.all()):
+        if isinstance(model, (Artifact, Content)):
+            continue
         try:
             if model.complete:
                 continue
