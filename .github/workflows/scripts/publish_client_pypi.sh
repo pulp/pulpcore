8f1781ade82665207e2f2cd0dd7438f9b62c46e7
pulpbot <pulp-infra@redhat.com>
2024-05-21 08:20:49 +0000

    Update CI files
    [noissue]

--- .github/workflows/scripts/publish_client_pypi.sh
+++ .github/workflows/scripts/publish_client_pypi.sh
@@ -24,10 +24,8 @@ RESPONSE="$(curl --write-out '%{http_code}' --silent --output /dev/null "https:/
 if [ "$RESPONSE" == "200" ];
 then
   echo "pulpcore client $VERSION has already been released. Skipping."
-  exit
+else
+  twine upload -u __token__ -p "$PYPI_API_TOKEN" \
+  "dist/pulpcore_client-$VERSION-py3-none-any.whl" \
+  "dist/pulpcore-client-$VERSION.tar.gz"
 fi
-
-twine upload -u __token__ -p "$PYPI_API_TOKEN" \
-"dist/pulpcore_client-$VERSION-py3-none-any.whl" \
-"dist/pulpcore-client-$VERSION.tar.gz" \
-;
