0b15b5da975dc738e5411018d5c6f5bd77ba3750
pulpbot <pulp-infra@redhat.com>
2024-06-24 12:17:34 +0000

    Update CI files
    [noissue]

--- .github/workflows/scripts/script.sh
+++ .github/workflows/scripts/script.sh
@@ -65,7 +65,9 @@ then
     echo $item
     COMPONENT="$(jq -r '.component' <<<"$item")"
     VERSION="$(jq -r '.version' <<<"$item")"
-    MODULE="$(jq -r '.module' <<<"$item")"
+    # On older status endpoints, the module was not provided, but the package should be accurate
+    # there, because we did not merge plugins into pulpcore back then.
+    MODULE="$(jq -r '.module // (.package|gsub("-"; "_"))' <<<"$item")"
     PACKAGE="${MODULE%%.*}"
     cmd_prefix pulpcore-manager openapi --bindings --component "${COMPONENT}" > api.json
     ./gen-client.sh api.json "${COMPONENT}" python "${PACKAGE}"
@@ -79,7 +81,9 @@ else
     echo $item
     COMPONENT="$(jq -r '.component' <<<"$item")"
     VERSION="$(jq -r '.version' <<<"$item")"
-    MODULE="$(jq -r '.module' <<<"$item")"
+    # On older status endpoints, the module was not provided, but the package should be accurate
+    # there, because we did not merge plugins into pulpcore back then.
+    MODULE="$(jq -r '.module // (.package|gsub("-"; "_"))' <<<"$item")"
     PACKAGE="${MODULE%%.*}"
     cmd_prefix pulpcore-manager openapi --bindings --component "${COMPONENT}" > api.json
     ./gen-client.sh api.json "${COMPONENT}" python "${PACKAGE}"
