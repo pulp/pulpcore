cc5f08b24dfe8dd78b8d358aadcad0522ad91ae4
pulpbot <pulp-infra@redhat.com>
2024-07-02 18:26:02 +0000

    Bump version: 3.49.13 → 3.49.14.dev

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as requirements:
 
 setup(
     name="pulpcore",
-    version="3.49.13",
+    version="3.49.14.dev",
     description="Pulp Django Application and Related Modules",
     long_description=long_description,
     long_description_content_type="text/markdown",
