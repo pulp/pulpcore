e81f61cb911a4617965b3c49c19f3891cac46481
Grant Gainey <ggainey@redhat.com>
2024-07-12 13:25:58 -0400

    Fix urlize_quoted_hrefs() to make links again.
    Also now correctly escapes JSON values for the web.
    
    fixes #5563.
    
    (cherry picked from commit 672a6233d210f62fc4c407531461d6e13012ab67)

---
+++ pulpcore/tests/unit/test_pulp_urls.py
@@ -0,0 +1,55 @@
+from django.conf import settings
+
+from pulpcore.app.templatetags import pulp_urls
+
+
+def test_urlize_quoted_hrefs_basic_url():
+    """
+    text starts with API_ROOT, defaults. Should be made clickable
+    """
+    txt = settings.V3_API_ROOT + "foo/bar/"
+    ret = pulp_urls.urlize_quoted_hrefs(txt)
+    assert ret == f'<a href="{txt}" rel="nofollow">{txt}</a>'
+
+
+def test_urlize_quoted_hrefs_nofollow():
+    """
+    text starts with API_ROOT, defaults. Should be made clickable
+    """
+    txt = settings.V3_API_ROOT + "foo/bar/"
+    ret = pulp_urls.urlize_quoted_hrefs(txt, nofollow=False)
+    assert ret == f'<a href="{txt}">{txt}</a>'
+
+
+def test_urlize_quoted_hrefs_trim():
+    """
+    text starts with API_ROOT, defaults. Should be made clickable
+    """
+    txt = settings.V3_API_ROOT + "foo/bar/"
+    trim_txt = txt[0] + "..."
+    ret = pulp_urls.urlize_quoted_hrefs(txt, trim_url_limit=4)
+    assert ret == f'<a href="{txt}" rel="nofollow">{trim_txt}</a>'
+
+
+def test_urlize_quoted_hrefs_basic_url_xss():
+    """
+    text starts with API_ROOT, includes XSS, defaults. Should be made clickable, escape XSS
+    """
+    txt = settings.V3_API_ROOT + "foo/bar/<script>alert('ALERT!')</script>blech/"
+    escapified_linked_text = (
+        '<a href="' + settings.V3_API_ROOT + "foo/bar/"
+        '%3Cscript%3Ealert(&#x27;ALERT!&#x27;)%3C/script%3Eblech/" '
+        'rel="nofollow">' + settings.V3_API_ROOT + "foo/bar/&lt;script&gt;"
+        "alert(&#x27;ALERT!&#x27;)&lt;/script&gt;blech/</a>"
+    )
+    ret = pulp_urls.urlize_quoted_hrefs(txt)
+    assert ret == escapified_linked_text
+
+
+def test_urlize_quoted_hrefs_basic_escape():
+    """
+    text contains XSS. Expect escaped
+    """
+    txt = "foo/bar/<script>alert('ALERT!')</script>blech/"
+    ret = pulp_urls.urlize_quoted_hrefs(txt)
+    assert ret == "foo/bar/&lt;script&gt;alert(&#x27;ALERT!&#x27;)&lt;/script&gt;blech/"
