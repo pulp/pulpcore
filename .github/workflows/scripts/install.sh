42ac3d6a76b16e2161786bd5ec944d96563a7c83
pulpbot <pulp-infra@redhat.com>
2024-07-15 15:09:14 +0000

    Update CI files
    [noissue]

--- .github/workflows/scripts/install.sh
+++ .github/workflows/scripts/install.sh
@@ -56,6 +56,11 @@ if [[ -f ../../ci_requirements.txt ]]; then
     ci_requirements: true
 VARSYAML
 fi
+if [ "$TEST" = "pulp" ]; then
+  cat >> vars/main.yaml << VARSYAML
+    upperbounds: true
+VARSYAML
+fi
 if [ "$TEST" = "lowerbounds" ]; then
   cat >> vars/main.yaml << VARSYAML
     lowerbounds: true
