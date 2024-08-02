3b72e16d9ffaa6a54c2eef50da1cfc2c5d71acd9
Matthias Dellweg <mdellweg@redhat.com>
2024-08-02 17:18:25 +0200

    Fix urlize for pulp api hrefs
    Instead of carrying a copy of the whole api template, we just overwrite
    the branding related blocks. Also we monkeypatch django's urlize to know
    about pulp api hrefs.
    
    fixes #5664

--- pulpcore/tests/unit/test_pulp_urls.py
+++ pulpcore/tests/unit/test_pulp_urls.py
@@ -1,58 +1,110 @@
 import pytest
-from django.conf import settings
+import json
 
 from pulpcore.app.templatetags import pulp_urls
 
-pytestmark = pytest.mark.usefixtures("fake_domain")
+
+@pytest.fixture(autouse=True)
+def api_root(settings):
+    settings.DOMAIN_ENABLED = True
+    # TODO: dynaconf lazy settings...
+    # settings.API_ROOT = "/baz/"
+    settings.V3_API_ROOT = "/baz/api/v3/"
 
 
-def test_urlize_quoted_hrefs_basic_url():
+def test_urlize_basic_url():
     """
-    text starts with API_ROOT, defaults. Should be made clickable
+    Text starts with V3_API_ROOT. Should be made clickable.
     """
-    txt = settings.V3_API_ROOT + "foo/bar/"
-    ret = pulp_urls.urlize_quoted_hrefs(txt)
+    txt = "/baz/api/v3/foo/bar/"
+    ret = pulp_urls.urlize(txt)
     assert ret == f'<a href="{txt}" rel="nofollow">{txt}</a>'
 
 
-def test_urlize_quoted_hrefs_nofollow():
+def test_urlize_quoted_href():
     """
-    text starts with API_ROOT, defaults. Should be made clickable
+    Text contains V3_API_ROOT in quotes. Should be made clickable.
     """
-    txt = settings.V3_API_ROOT + "foo/bar/"
-    ret = pulp_urls.urlize_quoted_hrefs(txt, nofollow=False)
-    assert ret == f'<a href="{txt}">{txt}</a>'
-
-
-def test_urlize_quoted_hrefs_trim():
-    """
-    text starts with API_ROOT, defaults. Should be made clickable
-    """
-    txt = settings.V3_API_ROOT + "foo/bar/"
-    trim_txt = txt[0] + "..."
-    ret = pulp_urls.urlize_quoted_hrefs(txt, trim_url_limit=4)
-    assert ret == f'<a href="{txt}" rel="nofollow">{trim_txt}</a>'
-
-
-def test_urlize_quoted_hrefs_basic_url_xss():
-    """
-    text starts with API_ROOT, includes XSS, defaults. Should be made clickable, escape XSS
-    """
-    txt = settings.V3_API_ROOT + "foo/bar/<script>alert('ALERT!')</script>blech/"
-    escapified_linked_text = (
-        '<a href="' + settings.V3_API_ROOT + "foo/bar/"
-        '%3Cscript%3Ealert(&#x27;ALERT!&#x27;)%3C/script%3Eblech/" '
-        'rel="nofollow">' + settings.V3_API_ROOT + "foo/bar/&lt;script&gt;"
-        "alert(&#x27;ALERT!&#x27;)&lt;/script&gt;blech/</a>"
+    txt = json.dumps({"pulp_href": "/baz/api/v3/foo/bar/"})
+    ret = pulp_urls.urlize(txt)
+    assert ret == (
+        '{&quot;pulp_href&quot;: &quot;<a href="/baz/api/v3/foo/bar/" '
+        'rel="nofollow">/baz/api/v3/foo/bar/</a>&quot;}'
     )
-    ret = pulp_urls.urlize_quoted_hrefs(txt)
+
+
+def test_urlize_mixed_bag():
+    """
+    Text contains lots of stuff in json.
+    """
+    txt = json.dumps(
+        {
+            "pulp_href": "/baz/api/v3/foo/bar/",
+            "related": "/baz/api/v3/other/",
+            "description": "Test XSS <script>alert('ALERT')</script>.",
+        }
+    )
+    ret = pulp_urls.urlize(txt)
+    assert ret == (
+        '{&quot;pulp_href&quot;: &quot;<a href="/baz/api/v3/foo/bar/" '
+        'rel="nofollow">/baz/api/v3/foo/bar/</a>&quot;, '
+        '&quot;related&quot;: &quot;<a href="/baz/api/v3/other/" '
+        'rel="nofollow">/baz/api/v3/other/</a>&quot;, '
+        "&quot;description&quot;: "
+        "&quot;Test XSS &lt;script&gt;alert(&#x27;ALERT&#x27;)&lt;/script&gt;.&quot;}"
+    )
+
+
+def test_urlize_quoted_href_no_autoescape():
+    """
+    Text contains V3_API_ROOT in quotes. Should be made clickable.
+    """
+    txt = json.dumps({"pulp_href": "/baz/api/v3/foo/bar/"})
+    ret = pulp_urls.urlize(txt, autoescape=False)
+    assert ret == (
+        '{"pulp_href": "<a href="/baz/api/v3/foo/bar/" rel="nofollow">/baz/api/v3/foo/bar/</a>"}'
+    )
+
+
+def test_urlize_url_xss_autoescape():
+    """
+    Text starts with API_ROOT, includes XSS. Should be made clickable, escape XSS.
+    """
+    txt = "/baz/api/v3/foo/bar/<script>alert('ALERT!')</script>blech/"
+    escapified_linked_text = (
+        '<a href="/baz/api/v3/foo/bar/" rel="nofollow">/baz/api/v3/foo/bar/</a>'
+        "&lt;script&gt;alert(&#x27;ALERT!&#x27;)&lt;/script&gt;blech/"
+    )
+    ret = pulp_urls.urlize(txt)
     assert ret == escapified_linked_text
 
 
-def test_urlize_quoted_hrefs_basic_escape():
+def test_urlize_url_xss_no_autoescape():
     """
-    text contains XSS. Expect escaped
+    Text starts with API_ROOT, includes XSS. Should be made clickable, not escape XSS.
+    """
+    txt = "/baz/api/v3/foo/bar/<script>alert('ALERT!')</script>blech/"
+    escapified_linked_text = (
+        '<a href="/baz/api/v3/foo/bar/" rel="nofollow">/baz/api/v3/foo/bar/</a>'
+        "<script>alert('ALERT!')</script>blech/"
+    )
+    ret = pulp_urls.urlize(txt, autoescape=False)
+    assert ret == escapified_linked_text
+
+
+def test_urlize_autoescape():
+    """
+    Text contains XSS. Expect escaped.
     """
     txt = "foo/bar/<script>alert('ALERT!')</script>blech/"
-    ret = pulp_urls.urlize_quoted_hrefs(txt)
+    ret = pulp_urls.urlize(txt)
     assert ret == "foo/bar/&lt;script&gt;alert(&#x27;ALERT!&#x27;)&lt;/script&gt;blech/"
+
+
+def test_urlize_no_autoescape():
+    """
+    Text contains XSS. Expect not escaped.
+    """
+    txt = "foo/bar/<script>alert('ALERT!')</script>blech/"
+    ret = pulp_urls.urlize(txt, autoescape=False)
+    assert ret == txt
