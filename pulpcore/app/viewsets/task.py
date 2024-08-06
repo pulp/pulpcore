4922cb22405d11c3a7d44f72c8db06ae76c94c3d
Gerrod Ubben <gerrodubben@gmail.com>
2024-08-06 16:11:03 -0400

    Fix incorrect RBAC permissions in object creation tasks
    fixes: #5683
    https://access.redhat.com/security/cve/cve-2024-7143
    (cherry picked from commit 20cffca49de5f3bfac44e0160c1f0fb262141ea5)

--- pulpcore/app/viewsets/task.py
+++ pulpcore/app/viewsets/task.py
@@ -142,6 +142,10 @@ class TaskViewSet(
             ],
         },
         "core.task_viewer": ["core.view_task"],
+        # This is a special role to designate the user who dispatched the task, it is assigned to
+        # the user on the object-level at task creation. It is not meant to be edited or manually
+        # added/removed from users.
+        "core.task_user_dispatcher": ["core.add_task"],
     }
 
     def get_serializer(self, *args, **kwargs):
@@ -190,6 +194,11 @@ class TaskViewSet(
                 ),
                 Prefetch("child_tasks", queryset=Task.objects.only("pk")),
             )
+        if self.action in ("add_role", "remove_role"):
+            if "core.task_user_dispatcher" == self.request.data.get("role"):
+                raise ValidationError(
+                    _("core.task_user_dispatcher can not be added/removed from a task.")
+                )
         return qs
 
     @extend_schema(
