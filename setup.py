e2f03715de68736b78942e5fa3aca98de2c318f7
pulpbot <pulp-infra@redhat.com>
2024-08-14 12:52:30 +0000

    Bump minor version [noissue]

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as requirements:
 
 setup(
     name="pulpcore",
-    version="3.58.0.dev",
+    version="3.59.0.dev",
     description="Pulp Django Application and Related Modules",
     long_description=long_description,
     long_description_content_type="text/markdown",
