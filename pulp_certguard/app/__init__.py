eada97c242e050df3e3e679dd74ffd1b298d906a
pulpbot <pulp-infra@redhat.com>
2024-09-18 00:07:21 +0000

    Bump version: 3.61.0 → 3.61.1.dev

--- pulp_certguard/app/__init__.py
+++ pulp_certguard/app/__init__.py
@@ -6,6 +6,6 @@ class PulpCertGuardPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_certguard.app"
     label = "certguard"
-    version = "3.61.0"
+    version = "3.61.1.dev"
     python_package_name = "pulpcore"
     domain_compatible = True
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as requirements:
 
 setup(
     name="pulpcore",
-    version="3.61.0",
+    version="3.61.1.dev",
     description="Pulp Django Application and Related Modules",
     long_description=long_description,
     long_description_content_type="text/markdown",
