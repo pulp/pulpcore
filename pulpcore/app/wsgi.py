d3aaf39b6b1b11caa12953fe4bd3c1db3b9a72cb
Grant Gainey <ggainey@redhat.com>
2024-10-06 02:09:18 -0400

    Fix WorkerNameMetricsExporter to not export unless otel is enabled.
    (cherry picked from commit f5c439c90a8c4043e978bbd09e0c6974151b2c05)
    
    Closes #6529

--- pulpcore/app/wsgi.py
+++ pulpcore/app/wsgi.py
@@ -7,6 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
+import os
 from django.core.wsgi import get_wsgi_application
 from opentelemetry.instrumentation.wsgi import OpenTelemetryMiddleware
 
@@ -16,7 +17,8 @@ if not using_pulp_api_worker.get(False):
     raise RuntimeError("This app must be executed using pulpcore-api entrypoint.")
 
 application = get_wsgi_application()
-application = OpenTelemetryMiddleware(application)
+if os.getenv("PULP_OTEL_ENABLED", "").lower() == "true":
+    application = OpenTelemetryMiddleware(application)
 
 from pulpcore.app.util import init_domain_metrics_exporter  # noqa: E402
 
