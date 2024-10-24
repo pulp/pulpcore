64ca6d91a6bb305ad7691ade4b9c0e82fe95fcc6
Pedro Brochado <pedropsb95@gmail.com>
2024-10-24 18:23:03 -0300

    Fix Any serializer type regression breaking Ruby bindings
    See https://github.com/pulp/pulp_rpm/issues/3639
    
    (cherry picked from commit 58425d791296752be427a43cda9df0d2403bc7bc)

--- pulpcore/app/serializers/fields.py
+++ pulpcore/app/serializers/fields.py
@@ -4,6 +4,8 @@ from gettext import gettext as _
 from urllib.parse import urljoin
 
 from django.conf import settings
+from drf_spectacular.utils import extend_schema_field
+from drf_spectacular.types import OpenApiTypes
 from rest_framework import serializers
 from rest_framework.fields import empty
 from rest_framework.reverse import reverse
@@ -18,6 +20,15 @@ def relative_path_validator(relative_path):
         raise serializers.ValidationError(
             _("Relative path can't start with '/'. {0}").format(relative_path)
         )
+
+
+@extend_schema_field(OpenApiTypes.OBJECT)
+class JSONDictField(serializers.JSONField):
+    """A drf JSONField override to force openapi schema to use 'object' type.
+
+    Not strictly correct, but we relied on that for a long time.
+    See: https://github.com/tfranzel/drf-spectacular/issues/1095
+    """
 
 
 class SingleContentArtifactField(RelatedField):
