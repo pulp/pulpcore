b18dc17591e1eb374f56fe5d451c44822885e23a
Ľuboš Mjachky <lmjachky@redhat.com>
2024-10-31 18:30:17 +0100

    TEST: Update pre_before_script.sh

--- .github/workflows/scripts/pre_before_script.sh
+++ .github/workflows/scripts/pre_before_script.sh
@@ -4,6 +4,5 @@ if [ "$TEST" = "azure" ]; then
     cmd_stdin_prefix bash -c "cat > /etc/nginx/pulp/api_root_rewrite.conf" < pulpcore/tests/functional/assets/api_root_rewrite.conf
     cmd_prefix bash -c "s6-rc -d change nginx"
     cmd_prefix bash -c "s6-rc -u change nginx"
-    cmd_stdin_prefix bash -c "cat > /var/lib/pulp/scripts/otel_server.py" < pulpcore/tests/functional/assets/otel_server.py
-    cmd_user_prefix nohup python3 /var/lib/pulp/scripts/otel_server.py &
+    cmd_prefix bash -c "until s6-svstat /var/run/service/nginx | grep -q \"up\"; do sleep 1; done"
 fi
