981af2ef7b01d19b6b40b2c3c1a5dcbbc0bdf7a7
Lubos Mjachky <lmjachky@redhat.com>
2024-10-31 18:10:04 +0100

    Remove functional tests for opentelemetry instrumentation

--- .github/workflows/scripts/install.sh
+++ .github/workflows/scripts/install.sh
@@ -120,7 +120,7 @@ if [ "$TEST" = "azure" ]; then
     command: "azurite-blob --blobHost 0.0.0.0"' vars/main.yaml
   sed -i -e '$a azure_test: true\
 pulp_scenario_settings: {"api_root_rewrite_header": "X-API-Root", "domain_enabled": true, "rest_framework__default_permission_classes": ["pulpcore.plugin.access_policy.DefaultAccessPolicy"]}\
-pulp_scenario_env: {"otel_bsp_max_export_batch_size": 1, "otel_bsp_max_queue_size": 1, "otel_exporter_otlp_endpoint": "http://localhost:4318", "otel_exporter_otlp_protocol": "http/protobuf", "otel_metric_export_interval": 800, "pulp_otel_enabled": "true"}\
+pulp_scenario_env: {}\
 ' vars/main.yaml
 fi
 
