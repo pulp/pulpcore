3cbfece3275f3c1bde4b7a591b80343ec83d6b59
Gerrod Ubben <gerrodubben@gmail.com>
2024-11-26 10:46:04 -0500

    Fix GetOrCreateSerializerMixin not working with pulp_domain
    (cherry picked from commit ed27c950a713ed71493cd39d0b0baceabed33b36)

--- pulpcore/app/serializers/base.py
+++ pulpcore/app/serializers/base.py
@@ -386,6 +386,8 @@ class GetOrCreateSerializerMixin:
             if default_values:
                 data.update(default_values)
             data.update(natural_key)
+            if "pulp_domain" in natural_key:
+                del data["pulp_domain"]
             serializer = cls(data=data)
             try:
                 serializer.is_valid(raise_exception=True)
@@ -402,6 +404,8 @@ class GetOrCreateSerializerMixin:
                 # validation failed with 400 'unique' error code only
                 result = cls.Meta.model.objects.get(**natural_key)
             try:
+                if "pulp_domain" in natural_key:
+                    serializer.validated_data["pulp_domain"] = natural_key["pulp_domain"]
                 result = result or serializer.create(serializer.validated_data)
             except IntegrityError:
                 # recover from a race condition, where another thread just created the object
