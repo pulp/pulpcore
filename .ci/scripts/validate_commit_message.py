82c22f2301416a674e6e3841a5709708048077be
pulpbot <pulp-infra@redhat.com>
2024-12-11 16:31:52 +0000

    Update CI files

--- .ci/scripts/validate_commit_message.py
+++ .ci/scripts/validate_commit_message.py
@@ -5,37 +5,57 @@
 #
 # For more info visit https://github.com/pulp/plugin_template
 
-import re
-import sys
-from pathlib import Path
-import subprocess
 import os
-import warnings
+import re
+import subprocess
+import sys
+import tomllib
+from pathlib import Path
+
 from github import Github
 
-CHANGELOG_EXTS = [".feature", ".bugfix", ".doc", ".removal", ".misc", ".deprecation"]
+with open("pyproject.toml", "rb") as fp:
+    PYPROJECT_TOML = tomllib.load(fp)
 KEYWORDS = ["fixes", "closes"]
+BLOCKING_REGEX = [
+    r"^DRAFT",
+    r"^WIP",
+    r"^NOMERGE",
+    r"^DO\s*NOT\s*MERGE",
+    r"^EXPERIMENT",
+    r"^FIXUP",
+    r"Apply suggestions from code review",
+]
+try:
+    CHANGELOG_EXTS = [
+        f".{item['directory']}" for item in PYPROJECT_TOML["tool"]["towncrier"]["type"]
+    ]
+except KeyError:
+    CHANGELOG_EXTS = [".feature", ".bugfix", ".doc", ".removal", ".misc"]
+NOISSUE_MARKER = "[noissue]"
 
 sha = sys.argv[1]
 message = subprocess.check_output(["git", "log", "--format=%B", "-n 1", sha]).decode("utf-8")
+
+if NOISSUE_MARKER in message:
+    sys.exit(f"Do not add '{NOISSUE_MARKER}' in the commit message.")
+
+if any((re.match(pattern, message, re.IGNORECASE) for pattern in BLOCKING_REGEX)):
+    sys.exit("This PR is not ready for consumption.")
 
 g = Github(os.environ.get("GITHUB_TOKEN"))
 repo = g.get_repo("pulp/pulpcore")
 
 
-def __check_status(issue):
+def check_status(issue):
     gi = repo.get_issue(int(issue))
     if gi.pull_request:
         sys.exit(f"Error: issue #{issue} is a pull request.")
-    if gi.closed_at and "cherry picked from commit" not in message:
-        warnings.warn(
-            "When backporting, use the -x flag to append a line that says "
-            "'(cherry picked from commit ...)' to the original commit message."
-        )
+    if gi.closed_at:
         sys.exit(f"Error: issue #{issue} is closed.")
 
 
-def __check_changelog(issue):
+def check_changelog(issue):
     matches = list(Path("CHANGES").rglob(f"{issue}.*"))
 
     if len(matches) < 1:
@@ -43,21 +63,20 @@ def __check_changelog(issue):
     for match in matches:
         if match.suffix not in CHANGELOG_EXTS:
             sys.exit(f"Invalid extension for changelog entry '{match}'.")
-        if match.suffix == ".feature" and "cherry picked from commit" in message:
-            sys.exit(f"Can not backport '{match}' as it is a feature.")
 
 
 print("Checking commit message for {sha}.".format(sha=sha[0:7]))
 
 # validate the issue attached to the commit
-regex = r"(?:{keywords})[\s:]+#(\d+)".format(keywords=("|").join(KEYWORDS))
-pattern = re.compile(regex, re.IGNORECASE)
-
-issues = pattern.findall(message)
+issue_regex = r"(?:{keywords})[\s:]+#(\d+)".format(keywords=("|").join(KEYWORDS))
+issues = re.findall(issue_regex, message, re.IGNORECASE)
+cherry_pick_regex = r"^\s*\(cherry picked from commit [0-9a-f]*\)\s*$"
+cherry_pick = re.search(cherry_pick_regex, message, re.MULTILINE)
 
 if issues:
-    for issue in pattern.findall(message):
-        __check_status(issue)
-        __check_changelog(issue)
+    for issue in issues:
+        if not cherry_pick:
+            check_status(issue)
+            check_changelog(issue)
 
 print("Commit message for {sha} passed.".format(sha=sha[0:7]))
