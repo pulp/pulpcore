416282f7d8c2f618e8e5ac4c6af380303494f240
Grant Gainey <ggainey@redhat.com>
2025-03-08 06:59:32 -0500

    Fixed a conflicting-migration problem with 0130 migration(s).

--- pulpcore/app/migrations/0131_distribution_checkpoint_publication_checkpoint.py
+++ pulpcore/app/migrations/0131_distribution_checkpoint_publication_checkpoint.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("core", "0129_content_pulp_labels"),
+        ("core", "0130_upstreampulp_policy"),
     ]
 
     operations = [
