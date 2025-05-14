42353144f7bf0e97fc8163ae954d227cc61b571e
Pedro Brochado <pedropsb95@gmail.com>
2025-05-14 18:02:32 -0300

    Revert runtime error removal in on-demand bugfix
    (cherry picked from commit 36bb6419921197a540a5a2d4b527796db2fc8e00)

--- pulpcore/content/handler.py
+++ pulpcore/content/handler.py
@@ -1177,7 +1177,7 @@ class Handler:
             )
             close_tcp_connection(request.transport._sock)
             REMOTE_CONTENT_FETCH_FAILURE_COOLDOWN = settings.REMOTE_CONTENT_FETCH_FAILURE_COOLDOWN
-            log.error(
+            raise RuntimeError(
                 f"Pulp tried streaming {remote_artifact.url!r} to "
                 "the client, but it failed checksum validation.\n\n"
                 "We can't recover from wrong data already sent so we are:\n"
@@ -1190,7 +1190,6 @@ class Handler:
                 "Learn more on <https://pulpproject.org/pulpcore/docs/user/learn/"
                 "on-demand-downloading/#on-demand-and-streamed-limitations>"
             )
-            return response
 
         if content_length := response.headers.get("Content-Length"):
             self._report_served_artifact_size(int(content_length))
