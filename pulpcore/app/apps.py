a77a2ff1f365e3fa1a8d15824007d790ddd535e2
pulpbot <pulp-infra@redhat.com>
2025-05-27 19:23:38 +0000

    Bump version: 3.28.39 → 3.28.40.dev

--- pulpcore/app/apps.py
+++ pulpcore/app/apps.py
@@ -234,7 +234,7 @@ class PulpAppConfig(PulpPluginAppConfig):
     label = "core"
 
     # The version of this app
-    version = "3.28.39"
+    version = "3.28.40.dev"
 
     # The python package name providing this app
     python_package_name = "pulpcore"
