a7ada87ac9223751ff3feab93ec9cd28a02ff287
Gerrod Ubben <gerrodubben@gmail.com>
2025-07-23 16:38:35 -0400

    Fix 'CSRF No Referer' issue in logout test

--- pulpcore/tests/functional/api/test_login.py
+++ pulpcore/tests/functional/api/test_login.py
@@ -1,5 +1,8 @@
 import http
 import pytest
+import uuid
+
+from urllib.parse import urlparse
 
 pytestmark = [pytest.mark.parallel]
 
@@ -20,27 +23,43 @@ def _fix_response_headers(monkeypatch, pulpcore_bindings):
 
 
 @pytest.fixture
-def session_user(pulpcore_bindings, gen_user, anonymous_user):
-    old_cookie = pulpcore_bindings.client.cookie
-    user = gen_user()
-    with user:
-        response = pulpcore_bindings.LoginApi.login_with_http_info()
-        if isinstance(response, tuple):
-            # old bindings
-            _, _, headers = response
-        else:
-            # new bindings
-            headers = response.headers
-    cookie_jar = http.cookies.SimpleCookie(headers["Set-Cookie"])
-    # Use anonymous_user to remove the basic auth header from the api client.
-    with anonymous_user:
-        pulpcore_bindings.client.cookie = "; ".join(
-            (f"{k}={v.value}" for k, v in cookie_jar.items())
-        )
-        # Weird: You need to pass the CSRFToken as a header not a cookie...
-        pulpcore_bindings.client.set_default_header("X-CSRFToken", cookie_jar["csrftoken"].value)
-        yield user
-        pulpcore_bindings.client.cookie = old_cookie
+def session_user(pulpcore_bindings, gen_user, bindings_cfg):
+    class SessionUser(gen_user):
+        def __enter__(self):
+            """
+            Mimic the behavior of a session user (aka what browsers do).
+
+            - Set auth to None so client will use the session cookie
+            - Set X-CSRFToken header since we are posting JSON instead of form data
+            (Django creates a hidden input field for the CSRF token when using forms)
+            - Set Origin and Host headers so Django CSRF middleware will allow the request
+            (Browsers send these headers and it is needed when using HTTPS)
+            """
+            self.old_cookie = pulpcore_bindings.client.cookie
+            super().__enter__()
+            response = pulpcore_bindings.LoginApi.login_with_http_info()
+            if isinstance(response, tuple):
+                # old bindings
+                _, _, headers = response
+            else:
+                # new bindings
+                headers = response.headers
+            cookie_jar = http.cookies.SimpleCookie(headers["Set-Cookie"])
+            self.cookie = "; ".join((f"{k}={v.value}" for k, v in cookie_jar.items()))
+            self.csrf_token = cookie_jar["csrftoken"].value
+            self.session_id = cookie_jar["sessionid"].value
+            bindings_cfg.username, bindings_cfg.password = None, None
+            pulpcore_bindings.client.cookie = self.cookie
+            pulpcore_bindings.client.set_default_header("X-CSRFToken", self.csrf_token)
+            pulpcore_bindings.client.set_default_header("Origin", bindings_cfg.host)
+            pulpcore_bindings.client.set_default_header("Host", urlparse(bindings_cfg.host).netloc)
+            return self
+
+        def __exit__(self, exc_type, exc_value, traceback):
+            super().__exit__(exc_type, exc_value, traceback)
+            pulpcore_bindings.client.cookie = self.old_cookie
+
+    return SessionUser
 
 
 def test_login_read_denies_anonymous(pulpcore_bindings, anonymous_user):
@@ -83,20 +102,31 @@ def test_login_sets_session_cookie(pulpcore_bindings, gen_user):
     assert cookie_jar["csrftoken"].value != ""
 
 
-def test_session_cookie_is_authorization(pulpcore_bindings, anonymous_user, session_user):
-    result = pulpcore_bindings.LoginApi.login_read()
-    assert result.username == session_user.username
+def test_session_cookie_is_authorization(pulpcore_bindings, session_user):
+    with session_user() as user:
+        result = pulpcore_bindings.LoginApi.login_read()
+        assert result.username == user.username
+        assert pulpcore_bindings.client.configuration.username is None
+
+
+def test_session_cookie_object_create(pulpcore_bindings, session_user, gen_object_with_cleanup):
+    with session_user(model_roles=["core.rbaccontentguard_creator"]):
+        assert pulpcore_bindings.client.configuration.username is None
+        gen_object_with_cleanup(pulpcore_bindings.ContentguardsRbacApi, {"name": str(uuid.uuid4())})
 
 
 def test_logout_removes_sessionid(pulpcore_bindings, session_user):
-    response = pulpcore_bindings.LoginApi.logout_with_http_info()
-    if isinstance(response, tuple):
-        # old bindings
-        _, status_code, headers = response
-    else:
-        # new bindings
-        status_code = response.status_code
-        headers = response.headers
+    with session_user() as user:
+        assert user.session_id != ""
+        assert user.session_id in pulpcore_bindings.client.cookie
+        response = pulpcore_bindings.LoginApi.logout_with_http_info()
+        if isinstance(response, tuple):
+            # old bindings
+            _, status_code, headers = response
+        else:
+            # new bindings
+            status_code = response.status_code
+            headers = response.headers
     assert status_code == 204
     cookie_jar = http.cookies.SimpleCookie(headers["Set-Cookie"])
     assert cookie_jar["sessionid"].value == ""
