3ef34c179e218de254dd1c74fd7463ea364f2e75
Matthias Dellweg <mdellweg@redhat.com>
2025-08-01 14:27:58 +0200

    Fix azure ci being flakey for restarting nginx
    Instead of restarting a service in the container, put the proper
    configuration in place when generating the image.

--- .github/workflows/scripts/pre_before_script.sh
+++
@@ -1,9 +0,0 @@
-set -euv
-
-if [ "$TEST" = "azure" ]; then
-    cmd_stdin_prefix bash -c "cat > /etc/nginx/pulp/api_root_rewrite.conf" < pulpcore/tests/functional/assets/api_root_rewrite.conf
-    cmd_prefix bash -c "s6-rc -d change nginx"
-    cmd_prefix bash -c "s6-rc -u change nginx"
-    # it is necessary to wait for a short period until the service starts; otherwise, any subsequent requests to Pulp's API endpoints will fail
-    sleep 1
-fi
