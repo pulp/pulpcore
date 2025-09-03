7d23bc069722dbd79d9d200901002095edcb1df4
pulpbot <pulp-infra@redhat.com>
2025-09-03 10:17:23 +0000

    Bump minor version

--- pulp_file/app/__init__.py
+++ pulp_file/app/__init__.py
@@ -8,6 +8,6 @@ class PulpFilePluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_file.app"
     label = "file"
-    version = "3.88.0.dev"
+    version = "3.89.0.dev"
     python_package_name = "pulpcore"
     domain_compatible = True
