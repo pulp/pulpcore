283d56fe3cc1ed0a3fd75dfb861584c9876ccc9c
pulpbot <pulp-infra@redhat.com>
2025-10-28 17:43:22 +0000

    Bump minor version

--- CHANGES/7024.feature
+++
@@ -1,3 +0,0 @@
-Add MAX_CONCURRENT_CONTENT as a global setting.
-During sync, this setting will be used to determine the size of batch of
-content being processed in one go. Default value is 200.
--- pulp_certguard/app/__init__.py
+++ pulp_certguard/app/__init__.py
@@ -6,6 +6,6 @@ class PulpCertGuardPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_certguard.app"
     label = "certguard"
-    version = "3.92.0.dev"
+    version = "3.93.0.dev"
     python_package_name = "pulpcore"
     domain_compatible = True
