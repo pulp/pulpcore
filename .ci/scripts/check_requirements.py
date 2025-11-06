bba5994b0ac997b553bd2eece913abec345dfc8d
Pedro Brochado <pedropsb95@gmail.com>
2025-11-06 14:42:28 -0300

    experiment: try unreleased dynaconf 3.3.0

--- .ci/scripts/check_requirements.py
+++ .ci/scripts/check_requirements.py
@@ -14,7 +14,7 @@ CHECK_MATRIX = [
     ("pyproject.toml", True, True, True),
     ("requirements.txt", True, True, True),
     ("dev_requirements.txt", False, True, False),
-    ("ci_requirements.txt", False, True, True),
+    # ("ci_requirements.txt", False, True, True),
     ("doc_requirements.txt", False, True, False),
     ("lint_requirements.txt", False, True, True),
     ("unittest_requirements.txt", False, True, True),
