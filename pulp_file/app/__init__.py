c1b6d52a09edd08dc2ec535c29d2d7d1ae6cefea
pulpbot <pulp-infra@redhat.com>
2025-12-17 17:00:38 +0000

    Bump version: 3.85.6 → 3.85.7.dev

--- pulp_file/app/__init__.py
+++ pulp_file/app/__init__.py
@@ -8,6 +8,6 @@ class PulpFilePluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_file.app"
     label = "file"
-    version = "3.85.6"
+    version = "3.85.7.dev"
     python_package_name = "pulpcore"
     domain_compatible = True
