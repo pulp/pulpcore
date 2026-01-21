5651b63ef56e97053adb997195b2f996f20e8873
pulpbot <pulp-infra@redhat.com>
2026-01-21 12:41:12 +0000

    Release 3.73.23

--- pulp_certguard/app/__init__.py
+++ pulp_certguard/app/__init__.py
@@ -6,6 +6,6 @@ class PulpCertGuardPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_certguard.app"
     label = "certguard"
-    version = "3.73.23.dev"
+    version = "3.73.23"
     python_package_name = "pulpcore"
     domain_compatible = True
