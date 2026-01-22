2ae65b3cc2ef10ae632c9842a7e304fdd7260053
Gerrod Ubben <gerrodubben@gmail.com>
2026-01-22 14:44:20 -0500

    Add support for rename of S3 storage backend
    fixes: #7228
    (cherry picked from commit 0d632d495492d885532b54f5ec7e425ee729f374)

--- pulpcore/tests/unit/serializers/test_domain.py
+++ pulpcore/tests/unit/serializers/test_domain.py
@@ -30,6 +30,7 @@ def _no_validate_storage_backend(monkeypatch):
     params=[
         "pulpcore.app.models.storage.FileSystem",
         "storages.backends.s3boto3.S3Boto3Storage",
+        "storages.backends.s3.S3Storage",
         "storages.backends.azure_storage.AzureStorage",
     ]
 )
@@ -41,7 +42,10 @@ def storage_class(request):
 def serializer_class(storage_class):
     if storage_class == "pulpcore.app.models.storage.FileSystem":
         return FileSystemSettingsSerializer
-    elif storage_class == "storages.backends.s3boto3.S3Boto3Storage":
+    elif storage_class in (
+        "storages.backends.s3boto3.S3Boto3Storage",
+        "storages.backends.s3.S3Storage",
+    ):
         return AmazonS3SettingsSerializer
     elif storage_class == "storages.backends.azure_storage.AzureStorage":
         return AzureSettingsSerializer
@@ -51,7 +55,10 @@ def serializer_class(storage_class):
 def required_settings(storage_class):
     if storage_class == "pulpcore.app.models.storage.FileSystem":
         return {"location": "/var/lib/pulp/media/"}
-    elif storage_class == "storages.backends.s3boto3.S3Boto3Storage":
+    elif storage_class in (
+        "storages.backends.s3boto3.S3Boto3Storage",
+        "storages.backends.s3.S3Storage",
+    ):
         return {"access_key": "testing", "bucket_name": "test"}
     elif storage_class == "storages.backends.azure_storage.AzureStorage":
         return {"account_name": "test", "account_key": "secret", "azure_container": "test"}
@@ -60,7 +67,10 @@ def required_settings(storage_class):
 @pytest.fixture
 def extra_required_settings(storage_class):
     """For fields required in the serializer's validate, but not on the field itself."""
-    if storage_class == "storages.backends.s3boto3.S3Boto3Storage":
+    if storage_class in (
+        "storages.backends.s3boto3.S3Boto3Storage",
+        "storages.backends.s3.S3Storage",
+    ):
         return {"secret_key": "secret"}
     return {}
 
@@ -125,6 +135,29 @@ def test_using_setting_names(storage_class, serializer_class, all_settings):
 
     storage_settings = serializer.validated_data["storage_settings"]
     assert storage_settings == all_settings
+
+
+@pytest.mark.django_db
+def test_cloudfront_s3_storage_settings(storage_class, required_settings):
+    if storage_class not in (
+        "storages.backends.s3boto3.S3Boto3Storage",
+        "storages.backends.s3.S3Storage",
+    ):
+        pytest.skip("This test only make sense when using S3 as storage backend.")
+
+    domain = SimpleNamespace(storage_class=storage_class, **MIN_DOMAIN_SETTINGS)
+    data = {
+        "storage_settings": {
+            "secret_key": "secret_key",
+            "custom_domain": "custom_domain.cloudfront.net",
+            "cloudfront_key_id": "key_id",
+            "cloudfront_key": "cloudfront_key",
+            **required_settings,
+        }
+    }
+    serializer = DomainSerializer(domain, data=data, partial=True)
+
+    assert serializer.is_valid(raise_exception=True)
 
 
 class DomainSettingsBaseMixin:
