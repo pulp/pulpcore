de7749e0babcf181b963d552e5b79ee563082210
pulpbot <pulp-infra@redhat.com>
2026-05-05 18:44:52 +0000

    Release 3.63.37

--- pulpcore/app/apps.py
+++ pulpcore/app/apps.py
@@ -242,7 +242,7 @@ class PulpAppConfig(PulpPluginAppConfig):
     label = "core"
 
     # The version of this app
-    version = "3.63.37.dev"
+    version = "3.63.37"
 
     # The python package name providing this app
     python_package_name = "pulpcore"
