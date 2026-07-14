02e9242c59450634c51bd5b7605e2d8043793383
Matthias Dellweg <mdellweg@redhat.com>
2026-07-14 17:57:15 +0200

    Remove insecure default Django secret

--- pulpcore/app/serializers/importer.py
+++ pulpcore/app/serializers/importer.py
@@ -1,10 +1,11 @@
 import os
 from gettext import gettext as _
 
+from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import serializers
 
-from pulpcore.app import models, settings
+from pulpcore.app import models
 from pulpcore.app.serializers import (
     DetailIdentityField,
     DomainUniqueValidator,
