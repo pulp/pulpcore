8e63be558e99cf2ca8bce65f47a726008b23ba62
pulpbot <pulp-infra@redhat.com>
2026-09-20 17:33:06 +0000

    Update CI files

--- .github/workflows/scripts/script.sh
+++ .github/workflows/scripts/script.sh
@@ -72,7 +72,7 @@ pushd ../pulp-openapi-generator
       rm -rf "./${PACKAGE}-client"
       ./gen-client.sh "${COMPONENT}-api.json" "${COMPONENT}" python "${PACKAGE}"
       pushd "${PACKAGE}-client"
-        python -m build
+        uv build
       popd
     else
       if [ ! -f "${PACKAGE}-client/dist/${PACKAGE}_client-${VERSION}-py3-none-any.whl" ]
