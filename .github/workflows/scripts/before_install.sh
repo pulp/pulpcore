dd3dab1a8b2bacf0548aad4b4965ad0c5fdd2d2f
pulpbot <pulp-infra@redhat.com>
2026-09-20 17:33:19 +0000

    Update CI files

--- .github/workflows/scripts/before_install.sh
+++ .github/workflows/scripts/before_install.sh
@@ -23,7 +23,7 @@ if [ -f .github/workflows/scripts/pre_before_install.sh ]; then
   source .github/workflows/scripts/pre_before_install.sh
 fi
 
-COMPONENT_VERSION="$(bump-my-version show current_version | tail -n -1 | python -c 'from packaging.version import Version; print(Version(input()))')"
+COMPONENT_VERSION="$(python -c 'import tomllib; from packaging.version import Version; print(Version(tomllib.load(open("pyproject.toml", "rb"))["tool"]["bumpversion"]["current_version"]))')"
 COMPONENT_SOURCE="./pulpcore/dist/pulpcore-${COMPONENT_VERSION}-py3-none-any.whl"
 if [ "$TEST" = "s3" ]; then
   COMPONENT_SOURCE="${COMPONENT_SOURCE}[s3]"
